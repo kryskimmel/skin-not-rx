4124babe9e089fa899ef6c3ed3d2bec82001f39b
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-07 22:09:14 -0600

    Added SplashPage component

---
+++ react-vite/src/components/SplashPage/SplashPage.jsx
@@ -0,0 +1,12 @@
+import "./SplashPage.css";
+
+function SplashPage() {
+    return (
+        <>
+        <h1>Skin-not-Rx</h1>
+        <p>Your personal skincare tracker</p>
+        </>
+    )
+};
+
+export default SplashPage;
