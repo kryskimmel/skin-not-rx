12388fef5fc0bbba20b315a7343fede18d3c8ec9
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-09 16:42:25 -0600

    Added models to file

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
-from .db import db
+from .db import db, environment, SCHEMA
 from .user import User
-from .db import environment, SCHEMA
+from .product import Product
+from .product_image import Product_Image
+from .favorite_product import Favorite_Product
+from .collection import Collection
+from .favorite_collection import Favorite_Collection
