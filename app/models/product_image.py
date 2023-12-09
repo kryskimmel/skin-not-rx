b402e38a804442525f4de25cc51652debe3d1ca4
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-09 14:35:39 -0600

    Added Product_Image model

--- app/models/product_image.py
+++ app/models/product_image.py
@@ -1,19 +1,19 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
-from db import Column, Integer, String, ForeignKey, DateTime
+from db import Column, Integer, String, Boolean, ForeignKey, DateTime
 from datetime import datetime
 # from flask_login import UserMixin
 
 
-class Collection(db.Model):
-    __tablename__ = 'collections'
+class Product_Image(db.Model):
+    __tablename__ = 'product_images'
 
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
     id = Column(Integer, primary_key=True)
-    name = Column(String(50), nullable=False, unique=True)
-    user_id = Column(Integer, ForeignKey(add_prefix_for_prod('users.id')))
     product_id = Column(Integer, ForeignKey(add_prefix_for_prod('products.id')))
+    preview = Column(Boolean, nullable=False)
+    image_url = Column(String(255), nullable=False)
 
     created_at = Column(DateTime, default=datetime.utcnow)
     updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
@@ -23,7 +23,7 @@ class Collection(db.Model):
     def to_dict(self):
         return {
             'id': self.id,
-            'name': self.name,
-            'user_id': self.user_id,
             'product_id': self.product_id,
+            'preview': self.preview,
+            'image_url': self.image_url
         }
