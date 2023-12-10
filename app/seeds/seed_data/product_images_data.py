b4305d7359cf1afb9f8c7c392c4ac937e78a9436
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-10 00:34:21 -0600

    Added product images seed data file

---
+++ app/seeds/seed_data/product_images_data.py
@@ -0,0 +1,55 @@
+product_images_data = [
+    # Neutrogena: Hydro Boost Water Gel with Hyaluronic Acid
+    { "product_id": 1, "preview": True, "image_url": "" },
+    { "product_id": 1, "preview": False, "image_url": "" },
+    # La Roche-Posay: Anthelios Melt-in Milk Sunscreen SPF 60
+    { "product_id": 2, "preview": True, "image_url": "" },
+    { "product_id": 2, "preview": False,"image_url": ""},
+    { "product_id": 2, "preview": False,"image_url": ""},
+    # Innisfree: Dewy Glow Jelly Cream with Jeju Cherry Blossom
+    { "product_id": 3, "preview": True,"image_url": ""},
+    { "product_id": 3, "preview": False,"image_url": ""},
+    # The Ordinary: Salicylic Acid 2% Solution
+    { "product_id": 4, "preview": True,"image_url": ""},
+    { "product_id": 4, "preview": False,"image_url": ""},
+    # Jack Black: Protein Booster Skin Serum
+    { "product_id": 5, "preview": True,"image_url": ""},
+    { "product_id": 5, "preview": False,"image_url": ""},
+    { "product_id": 5, "preview": False,"image_url": ""},
+    # Glow Recipe: Watermelon Glow AHA Night Treatment
+    { "product_id": 6, "preview": True,"image_url": ""},
+    # Saturday Skin: Intense Hydration Mask
+    { "product_id": 7, "preview": True,"image_url": ""},
+    # TONYMOLY: I'm Red Wine Pore Tightening Mask
+    { "product_id": 8, "preview": True,"image_url": ""},
+    { "product_id": 8, "preview": False,"image_url": ""},
+    # TONYMOLY: Intense Care Gold 24K Snail Eye Serum
+    { "product_id": 9, "preview": True,"image_url": ""},
+    # Shiseido: Wrinkle Smoothing Eye Cream
+    { "product_id": 10, "preview": True,"image_url": ""},
+    { "product_id": 10, "preview": False,"image_url": ""},
+    { "product_id": 10, "preview": False,"image_url": ""},
+    # The Ordinary: AHA 30% + BHA 2% Peeling Solution
+    { "product_id": 11, "preview": True,"image_url": ""},
+    # Thayers: Rose Petal Facial Toner
+    { "product_id": 12, "preview": True,"image_url": ""},
+    { "product_id": 12, "preview": False,"image_url": ""},
+    # Beauty of Joseon: Glow Serum: Propolis + Niacinamide
+    { "product_id": 13, "preview": True,"image_url": ""},
+    # Elizabeth Arden: Eight Hour Cream Intensive Lip Repair Balm
+    { "product_id": 14, "preview": True,"image_url": ""},
+    # COSRX: Advcanced Snail 96 Mucin Power Essence
+    { "product_id": 15, "preview": True,"image_url": ""},
+    # Tatcha: The Rice Wash
+    { "product_id": 16, "preview": True,"image_url": ""},
+    { "product_id": 16, "preview": False,"image_url": ""},
+    { "product_id": 16, "preview": False,"image_url": ""},
+    # Laneige: Lip Sleeping Mask
+    { "product_id": 17, "preview": True,"image_url": ""},
+    # Innisfree: Pore Clearing Clay Mask 2X
+    { "product_id": 18, "preview": True,"image_url": ""},
+    # Drunk Elephant: Beste™ No. 9 Jelly Cleanser
+    { "product_id": 19, "preview": True,"image_url": ""},
+    # Kiehls: Facial Fuel Daily Energizing Moisture Treatment for Men
+    { "product_id": 20, "preview": True,"image_url": ""},
+]
