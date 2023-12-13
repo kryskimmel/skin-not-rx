1c6bfbeb29d6e48cf07a104683e80115d29fbcae
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-12 22:12:54 -0600

    Completed add a product route

--- app/api/products_routes.py
+++ app/api/products_routes.py
@@ -1,10 +1,10 @@
-from flask import Blueprint, jsonify
-from flask_login import login_required
-from app.models import Product
+from flask import Blueprint, jsonify, request
+from flask_login import current_user
+from app.models import Product, Product_Image, db
 
 product_routes = Blueprint('products', __name__)
 
-
+# Get all products (Explore page)
 @product_routes.route('/explore', methods=['GET'])
 def explore_products():
     all_products = Product.query.all()
@@ -25,3 +25,32 @@ def explore_products():
         }
         products_list.append(product_info)
     return jsonify({'Products': products_list})
+
+# Add a product
+@product_routes.route('/', methods=['GET', 'POST'])
+def add_product():
+    data = request.get_json()
+    new_product = Product(
+        brand_name=data.get('brand_name'),
+        product_name=data.get('product_name'),
+        product_type=data.get('product_type'),
+        description=data.get('description'),
+        key_ingredients=data.get('key_ingredients'),
+        skin_conern=data.get('skin_concern'),
+        product_link=data.get('product_link'),
+        notes=data.get('notes'),
+        user_id=current_user.id
+    )
+    db.session.add(new_product)
+    db.session.commit()
+
+    newPreviewImage = Product_Image(
+        product_id=new_product.id,
+        preview=data.get('preview'),
+        image_url=data.get('image_url')
+    )
+    db.session.add(newPreviewImage)
+    db.session.commit()
+
+    product_and_product_img=new_product.to_dict()
+    return jsonify(product_and_product_img)
