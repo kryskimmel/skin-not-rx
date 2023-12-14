7917774832ad68a52cd6f6c1f6cae16fd8682955
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-13 18:54:01 -0600

    commented in users seed data

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -6,20 +6,20 @@ from sqlalchemy.sql import text
 def seed_users():
     demo = User(
         first_name='Demo', last_name='Lition', username='demo', email='demo@aa.io', password='password', profile_image='https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg', skin_type='Dry')
-    # vanessa = User(
-    #     first_name='Vanessa', last_name='Sanchez', username='vanessa1', email='vanessa@aa.io', password='password', profile_image='https://upload.wikimedia.org/wikipedia/commons/3/3d/Fesoj_-_Papilio_machaon_%28by%29.jpg', skin_type='Combination')
-    # travis = User(
-    #     first_name='Travis', last_name='Tee', username='travistee', email='travis@gmail.com', password='password', profile_image='https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg', skin_type='Dry')
-    # khloe = User(
-    #     first_name='Khloe', last_name='Chen', username='khloechen', email='khloe@gmail.com', password='password', profile_image='https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Starsinthesky.jpg/1920px-Starsinthesky.jpg', skin_type='Oily')
-    # yasmin = User(
-    #     first_name='Yasmin', last_name='Anderson', username='yasmin_xo', email='yasmin@gmail.com', password='password', profile_image='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Rosa_Precious_platinum.jpg/1920px-Rosa_Precious_platinum.jpg', skin_type='Acne-Prone')
+    vanessa = User(
+        first_name='Vanessa', last_name='Sanchez', username='vanessa1', email='vanessa@aa.io', password='password', profile_image='https://upload.wikimedia.org/wikipedia/commons/3/3d/Fesoj_-_Papilio_machaon_%28by%29.jpg', skin_type='Combination')
+    travis = User(
+        first_name='Travis', last_name='Tee', username='travistee', email='travis@gmail.com', password='password', profile_image='https://upload.wikimedia.org/wikipedia/commons/7/73/Lion_waiting_in_Namibia.jpg', skin_type='Dry')
+    khloe = User(
+        first_name='Khloe', last_name='Chen', username='khloechen', email='khloe@gmail.com', password='password', profile_image='https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/Starsinthesky.jpg/1920px-Starsinthesky.jpg', skin_type='Oily')
+    yasmin = User(
+        first_name='Yasmin', last_name='Anderson', username='yasmin_xo', email='yasmin@gmail.com', password='password', profile_image='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3a/Rosa_Precious_platinum.jpg/1920px-Rosa_Precious_platinum.jpg', skin_type='Acne-Prone')
 
     db.session.add(demo)
-    # db.session.add(vanessa)
-    # db.session.add(travis)
-    # db.session.add(khloe)
-    # db.session.add(yasmin)
+    db.session.add(vanessa)
+    db.session.add(travis)
+    db.session.add(khloe)
+    db.session.add(yasmin)
     db.session.commit()
 
 
