4edf0c2f4fa39b7669b45b460926a95c8c5543e9
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-15 00:02:06 -0600

    Completed favorite related routes

--- app/api/current_user_routes.py
+++ app/api/current_user_routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
-from app.models import User, Favorite_Product, Favorite_Collection, db
+from app.models import User, Favorite_Product, Favorite_Collection, Product, Collection, db
 
 current_user_routes = Blueprint('current', __name__)
 
@@ -29,9 +29,12 @@ def user(id):
 @current_user_routes.route('/favorites/products', method=['GET'])
 @login_required
 def view_favorites():
-    favorite_products = Favorite_Product.query.all()
-    favorite_products_list = []
+    favorite_products = Favorite_Product.filter_by(user_id=current_user.id).all()
 
+    if not favorite_products:
+        return {'message': 'You do not have any favorited products yet!'}
+
+    favorite_products_list = []
     for product in favorite_products:
         fave_product = {
             'id': product.id,
@@ -39,14 +42,6 @@ def view_favorites():
             'product_id': product.product_id
         }
         favorite_products_list.append(fave_product)
-
-    for collection in favorite_products:
-        fave_collection = {
-            'id': collection.id,
-            'user_id': current_user.id,
-            'collection_id': collection.collection_id
-        }
-        favorite_products_list.append(fave_collection)
     return jsonify({'Favorites': favorite_products_list})
 
 
@@ -54,9 +49,12 @@ def view_favorites():
 @current_user_routes.route('/favorites/collections', method=['GET'])
 @login_required
 def view_favorites():
-    favorite_collections = Favorite_Collection.query.all()
-    favorite_collections_list = []
+    favorite_collections = Favorite_Collection.filter_by(user_id=current_user.id).all()
 
+    if not favorite_collections:
+        return {'message': 'You do not have any favorited collections yet!'}
+
+    favorite_collections_list = []
     for collection in favorite_collections:
         fave_collection = {
             'id': collection.id,
@@ -67,14 +65,73 @@ def view_favorites():
     return jsonify({'Favorite Collections': favorite_collections_list})
 
 
-# Add a favorite
-@current_user_routes.route('/favorites', method=['POST'])
-def add_favorite():
-    pass
+# Add a product to favorites
+@current_user_routes.route('/favorites/products', method=['POST'])
+@login_required
+def add_favorite_product():
+    data = request.get_json()
+    match_product = Product.query.filter_by(id=data.get('product_id'))
+
+    if not match_product:
+        return {'message': 'Product could not be found'}, 404
+
+    new_favorite_product = Favorite_Product(
+        user_id=current_user.id, product_id=data.get('product_id')
+    )
+    db.session.add(new_favorite_product)
+    db.session.commit()
+    return new_favorite_product.to_dict(), 201
+
+
+# Add a collection to favorites
+@current_user_routes.route('/favorites/collections', method=['POST'])
+@login_required
+def add_favorite_collection():
+    data = request.get_json()
+    match_collection = Collection.query.filter_by(id=data.get('collection_id'))
+
+    if not match_collection:
+        return {'message': 'Collection could not be found'}, 404
+
+    new_favorite_collection = Favorite_Collection(
+        user_id=current_user.id, collection_id=data.get('collection_id')
+    )
+    db.session.add(new_favorite_collection)
+    db.session.commit()
+    return new_favorite_collection.to_dict(), 201
 
 
 # Delete a favorited product
+@current_user_routes.route('/favorites/products/<int:product_id>', methods=['DELETE'])
+@login_required
+def remove_favorite_product(product_id):
+    current_product_favorite = Favorite_Product.query.filter_by(user_id=current_user.id).filter_by(
+        product_id=product_id).first()
 
+    if not current_product_favorite:
+        return {'message': 'The selected product could not be deleted as it was not favorited'}, 404
+
+    if current_user.id == current_product_favorite.user_id:
+        db.session.delete(current_product_favorite)
+        db.session.commit()
+        return {'message': 'Successfully deleted.'}, 200
+    else:
+        return {'message': 'Forbidden'}, 403
 
 
 # Delete a favorited collection
+@current_user_routes.route('/favorites/collections/<int:collection_id>', methods=['DELETE'])
+@login_required
+def remove_favorite_collection(collection_id):
+    current_collection_favorite = Favorite_Collection.query.filter_by(user_id=current_user.id).filter_by(
+    collection_id=collection_id).first()
+
+    if not current_collection_favorite:
+        return {'message': 'The selected collection could not be deleted as it was not favorited'}, 404
+
+    if current_user.id == current_collection_favorite.user_id:
+        db.session.delete(current_collection_favorite)
+        db.session.commit()
+        return {'message': 'Successfully deleted.'}, 200
+    else:
+        return {'message': 'Forbidden'}, 403
