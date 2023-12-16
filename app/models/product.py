4c77163ed4c7d0586b45f5234e8f52bd5deb7049
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-16 15:23:39 -0600

    Modified column character lengeths for products table

--- app/models/product.py
+++ app/models/product.py
@@ -16,8 +16,8 @@ class Product(db.Model):
     product_type = Column(String(60), nullable=False)
     description = Column(Text(500), nullable=False)
     key_ingredients = Column(Text(500), nullable=True)
-    skin_concern = Column(String(200), nullable=False)
-    product_link = Column(String(200), nullable=True)
+    skin_concern = Column(String(300), nullable=False)
+    product_link = Column(String(500), nullable=True)
     notes = Column(Text(500), nullable=True)
 
     user_id = Column(Integer, ForeignKey(add_prefix_for_prod('users.id')))
