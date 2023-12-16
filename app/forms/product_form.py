77ee3c748b04723307a44e49a72300fe7452bd27
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-16 17:00:28 -0600

    Added min character validation

--- app/forms/product_form.py
+++ app/forms/product_form.py
@@ -15,12 +15,16 @@ def character_max_500(form, field):
     if len(field.data) > 500:
         raise ValidationError("Input must not exceed 500 characters.")
 
+def character_min_3(form, field):
+    if field.data and len(field.data) < 3:
+        raise ValidationError("Input must be at least 3 characters long.")
+
 class ProductForm(FlaskForm):
-    brand_name = StringField('brand_name', validators=[DataRequired(), character_max_60()])
-    product_name = StringField('product_name', validators=[DataRequired(), character_max_60()])
-    product_type = StringField('product_type', validators=[DataRequired(), character_max_60()])
-    description = StringField('description', validators=[DataRequired(), character_max_500()])
-    key_ingredients = StringField('key_ingredients', validators=[character_max_500()])
-    skin_concern = StringField('skin_concern', validators=[DataRequired(), character_max_300()])
-    product_link = StringField('product_link', validators=[character_max_500()])
-    notes = StringField('notes', validators=[character_max_500()])
+    brand_name = StringField('brand_name', validators=[DataRequired(), character_max_60])
+    product_name = StringField('product_name', validators=[DataRequired(), character_max_60])
+    product_type = StringField('product_type', validators=[DataRequired(), character_max_60])
+    description = StringField('description', validators=[DataRequired(), character_max_500])
+    key_ingredients = StringField('key_ingredients', validators=[character_max_500, character_min_3])
+    skin_concern = StringField('skin_concern', validators=[DataRequired(), character_max_300])
+    product_link = StringField('product_link', validators=[character_max_500, character_min_3])
+    notes = StringField('notes', validators=[character_max_500, character_min_3])
