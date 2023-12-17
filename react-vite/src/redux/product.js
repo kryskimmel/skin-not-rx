c38bb041facfea68a0027f70d9ad90b2d060ff3a
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-16 21:31:53 -0600

    updated thunks and reducer

--- react-vite/src/redux/product.js
+++ react-vite/src/redux/product.js
@@ -1,25 +1,72 @@
-const GET_PRODUCT = 'product/GET_PRODUCT';
+const GET_PRODUCTS = 'product/GET_PRODUCTS';
 const ADD_PRODUCT = 'product/ADD_PRODUCT';
 const EDIT_PRODUCT = 'product/EDIT_PRODUCT';
 const DELETE_PRODUCT = 'product/DELETE_PRODUCT';
 
 
-const getProduct = () => ({
-  type: GET_PRODUCT,
-  payload: product
+const getProducts = () => ({
+  type: GET_PRODUCTS,
+  payload: products
 });
 
 const addProduct = () => ({
   type: ADD_PRODUCT,
-  payload: product
+  payload: newProduct
 });
 
 const editProduct = () => ({
     type: EDIT_PRODUCT,
-    payload: product
+    payload: editedProduct
 });
 
 const deleteProduct = () => ({
     type: DELETE_PRODUCT,
-    payload: product
+    payload: deletedProduct
 })
+
+
+// GET ALL PRODUCTS
+export const getAllProducts = () => async (dispatch) => {
+    try {
+      const response = await fetch("/api/products/explore", {
+        method: "GET",
+      });
+      if (!response.ok) {
+        throw new Error(`There was an error fetching all existing products.`)
+      }
+      const allProducts = await response.json()
+      dispatch(getProducts(allProducts))
+      return allProducts;
+    } catch (error) {
+      throw new Error(`The following error occuring while attempting to fetch all existing products: ${error.message}`)
+    }
+};
+
+
+// Reducer
+const initialState = {allProducts:[], byId:{}}
+
+export default function reducer(state = initialState, action) {
+    let newState = {};
+
+    switch (action.type) {
+        case GET_PRODUCTS:
+            if (action.payload.Products) {
+                const byId = {};
+                action.payload.Products.forEach((product) => {
+                    byId[product.id] = product
+                });
+                newState = {
+                    allProducts: action.payload.Products,
+                    byId: byId
+                };
+                return newState;
+            } else {
+                newState = action.payload
+                return newState;
+            }
+        default:
+            return state;
+    }
+
+}
