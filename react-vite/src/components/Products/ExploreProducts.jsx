8ccda7106bc2ed78943affb46d19723c19b452ae
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-17 00:23:11 -0600

    Created directory to house product related files

---
+++ react-vite/src/components/Products/ExploreProducts.jsx
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+function ExploreProducts() {
+
+    return (
+        <>
+        hi
+        </>
+    )
+}
+
+export default ExploreProducts;
