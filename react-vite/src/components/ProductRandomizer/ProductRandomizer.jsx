54f9b80268939ccef199df79d78ebb12948237a1
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-17 11:05:53 -0600

    Edited form

--- react-vite/src/components/ProductRandomizer/ProductRandomizer.jsx
+++ react-vite/src/components/ProductRandomizer/ProductRandomizer.jsx
@@ -1,11 +1,40 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as productActions from "../../redux/product";
 import "./ProductRandomizer.css";
 
 function ProductRandomizer() {
+    const [selected, setSelected] = useState("");
+    const dispatch = useDispatch();
+    const allProducts = useSelector(state => state.product.allProducts);
+
+    console.log(allProducts)
+    console.log(selected)
+
+
+    useEffect(() => {
+        dispatch(productActions.getAllProducts())
+    }, [dispatch])
+
+    const handleSelect = (e) => {
+        setSelected(e.target.value)
+    }
+
+    const handleSubmit = () => {
+        if (selected) {
+            filtered = allProducts.filter(product_type === selected)
+            console.log('filtereddd', filtered)
+
+        }
+    }
+
+    console.log(selected)
+
     return (
         <div className="product-randomizer-container">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Looking for a new product to add to your routine?</label>
-                <select name="product-randomizer">
+                <select name="product-randomizer" onClick={handleSelect}>
                     <option value="cleanser">Cleanser</option>
                     <option value="exfoliator">Exfoliator</option>
                     <option value="treatment">Treatment</option>
@@ -18,7 +47,7 @@ function ProductRandomizer() {
                     <option value="eye-cream">Eye Cream</option>
                     <option value="lip-repair-protectant">Lip Repair & Protectant</option>
                 </select>
-                <button className="generate-button">Generate</button>
+                <button className="generate-button" onClick={()=>{console.log('selected value:', selected)}}>Generate</button>
             </form>
         </div>
     )
