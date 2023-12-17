0d55a483a6521d4cd0413e7eea8d54b9af44236d
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-17 00:21:41 -0600

    Inserted ExploreButton component

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -1,18 +1,27 @@
 import { NavLink } from "react-router-dom";
 import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
+import ExploreButton from "./ExploreButton";
 
 function Navigation() {
   return (
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
+    <div className="nav-container">
+      <ul className="nav-left">
+        <li>
+          <NavLink to="/">Home</NavLink>
+        </li>
+      </ul>
+      <ul className="nav-right">
+        <li>
+          <ExploreButton />
+        </li>
+        <li>
+          <ProfileButton />
+        </li>
+      </ul>
 
-      <li>
-        <ProfileButton />
-      </li>
-    </ul>
+
+    </div>
   );
 }
 
