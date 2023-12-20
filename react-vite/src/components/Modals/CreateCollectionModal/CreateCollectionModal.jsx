169c154b537b93fb28fabfd137f35f579ef66159
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-20 17:50:37 -0600

    Created Modal for creating a collection

---
+++ react-vite/src/components/Modals/CreateCollectionModal/CreateCollectionModal.jsx
@@ -0,0 +1,19 @@
+import { useModal } from '../../../context/Modal';
+import "./CreateCollectionModal.css";
+
+function CreateCollectionModal() {
+    const { closeModal } = useModal();
+    return (
+        <>
+        <div className='create-collection-container'>
+            <form>
+                <h1>Create A Collection</h1>
+                <label>Collection Name</label>
+                <input type="text"/>
+            </form>
+        </div>
+        </>
+    )
+};
+
+export default CreateCollectionModal;
