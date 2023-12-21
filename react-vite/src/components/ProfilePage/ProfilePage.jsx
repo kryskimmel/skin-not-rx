55fe390f07cfb3fd555cd3c726a87d0c025267d4
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-21 16:12:32 -0600

    modified logic and css

--- react-vite/src/components/ProfilePage/ProfilePage.jsx
+++ react-vite/src/components/ProfilePage/ProfilePage.jsx
@@ -1,8 +1,36 @@
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import * as productActions from "../../redux/product";
 import "./ProfilePage.css";
 
 function ProfilePage() {
+    const dispatch = useDispatch();
+    const user = useSelector(store => store.session.user);
+    const productsByType = useSelector(store => store.product.byProductType);
+
+    useEffect(() => { dispatch(productActions.getAllProducts())}, [dispatch]);
+
+
+
+    console.log('products selector', productsByType)
+
     return (
         <>
+        <div className="profile-page-container">
+            <h1 className="profile-page-h1">{user.username.toUpperCase()}'S PROFILE</h1>
+            <div className="profile-page-collections-div">
+                {Object.entries(productsByType).map(([productType, products]) => (
+                <div key={productType} className="profile-page-collections-tile">
+                    <h2>{productType}</h2>
+                    {/* {products.map((product) => (
+                        <p key={product.id}>{product.name}</p>
+
+                    ))} */}
+                </div>
+                ))}
+            </div>
+
+        </div>
         </>
     )
 }
