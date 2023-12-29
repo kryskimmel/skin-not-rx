373fe0a00c8efce108ca31d1bffec8997f5d1671
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-28 20:45:41 -0600

    modified seed data

--- app/seeds/seed_data/collections_data.py
+++ app/seeds/seed_data/collections_data.py
@@ -1,49 +1,18 @@
-# 20 products seed data items
+# 3 collection seed data items
 collections_data = [
     {
         "name": "Let It Glow",
         "user_id": 1,
-        "product_id": 6
-    },
-    {
-        "name": "Let It Glow",
-        "user_id": 1,
-        "product_id":  13
-    },
-    {
-        "name": "Night Out Prep",
-        "user_id": 1,
-        "product_id": 8
-    },
-    {
-        "name": "Night Out Prep",
-        "user_id": 1,
-        "product_id": 1
-    },
-
-    {
-        "name": "Night Out Prep",
-        "user_id": 1,
-        "product_id": 16
-    },
-    {
-        "name": "It's Sunday",
-        "user_id": 2,
-        "product_id": 16
-    },
-    {
-        "name": "It's Sunday",
-        "user_id": 2,
-        "product_id": 18
-    },
-    {
-        "name": "It's Sunday",
-        "user_id": 2,
         "product_id": 5
     },
     {
+        "name": "Night Out Prep",
+        "user_id": 1,
+        "product_id": 7
+    },
+    {
         "name": "It's Sunday",
         "user_id": 2,
-        "product_id": 3
+        "product_id": 6
     },
 ]
