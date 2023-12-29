ae63d7863cc83c3080c0d20bd499bfb9ebc998cb
Krystal Kimmel <kabkimmel@gmail.com>
2023-12-29 01:31:38 -0600

    Added elements to component

--- react-vite/src/components/Modals/CreateCollectionModal/CreateCollectionModal.jsx
+++ react-vite/src/components/Modals/CreateCollectionModal/CreateCollectionModal.jsx
@@ -1,15 +1,21 @@
-// import { useModal } from '../../../context/Modal';
+import { useSelector } from "react-redux";
 import "./CreateCollectionModal.css";
 
 function CreateCollectionModal() {
-    // const { closeModal } = useModal();
+    const allProducts = useSelector(state => state.product.allProducts)
+
     return (
         <>
         <div className='create-collection-container'>
             <form>
                 <h1>Create A Collection</h1>
-                <label>Collection Name</label>
+                <label>Collection Name:</label>
                 <input type="text"/>
+                <label>Add Products:</label>
+                <select>
+                    {allProducts.map(product => <option key={product.id}>{product.brand_name.toUpperCase()} {product.product_name}</option>)}
+                </select>
+                <button className="create-collection-button">Create Collection</button>
             </form>
         </div>
         </>
