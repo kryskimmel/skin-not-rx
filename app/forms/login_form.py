df6c0fa22f9dde2e83b91e364d2e4a80b1e648f4
Krystal Kimmel <kabkimmel@gmail.com>
2024-01-01 20:50:58 -0600

    modified login form error message

--- app/forms/login_form.py
+++ app/forms/login_form.py
@@ -9,7 +9,7 @@ def user_exists(form, field):
     email = field.data
     user = User.query.filter(User.email == email).first()
     if not user:
-        raise ValidationError('Email provided not found.')
+        raise ValidationError('The email provided could not be found.')
 
 
 def password_matches(form, field):
@@ -18,9 +18,9 @@ def password_matches(form, field):
     email = form.data['email']
     user = User.query.filter(User.email == email).first()
     if not user:
-        raise ValidationError('No such user exists.')
+        raise ValidationError('The provided email or password was incorrect. Please try again.')
     if not user.check_password(password):
-        raise ValidationError('Password was incorrect.')
+        raise ValidationError('The provided email or password was incorrect. Please try again.')
 
 
 class LoginForm(FlaskForm):
