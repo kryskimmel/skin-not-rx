a4305dfd65b1a430e67db2c0ac862c3c2e44cff6
Krystal Kimmel <kabkimmel@gmail.com>
2024-01-01 21:24:38 -0600

    Added form fields

--- react-vite/src/components/Modals/SignupFormModal/SignupFormModal.jsx
+++ react-vite/src/components/Modals/SignupFormModal/SignupFormModal.jsx
@@ -6,10 +6,14 @@ import "./SignupForm.css";
 
 function SignupFormModal() {
   const dispatch = useDispatch();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [profileImage, setProfileImage] = useState("");
+  const [skinType, setSkinType] = useState("");
   const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
 
@@ -39,53 +43,92 @@ function SignupFormModal() {
   };
 
   return (
-    <>
+    <div className="signup-form-wrapper">
       <h1>Sign Up</h1>
       {errors.server && <p>{errors.server}</p>}
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input
-            type="text"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </label>
-        {errors.email && <p>{errors.email}</p>}
-        <label>
-          Username
+      <form onSubmit={handleSubmit} className="signup-form-div">
+        <label>First Name</label>
+            <input
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              required
+            />
+          {errors.firstName && <p>{errors.firstName}</p>}
+
+
+          <label>Last Name</label>
+            <input
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              required
+            />
+          {errors.lastName && <p>{errors.lastName}</p>}
+
+
+          <label>Username</label>
           <input
             type="text"
             value={username}
             onChange={(e) => setUsername(e.target.value)}
             required
           />
-        </label>
         {errors.username && <p>{errors.username}</p>}
-        <label>
-          Password
+
+
+        <label>Email</label>
+          <input
+            type="text"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        {errors.email && <p>{errors.email}</p>}
+
+
+        <label>Password</label>
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
           />
-        </label>
         {errors.password && <p>{errors.password}</p>}
-        <label>
-          Confirm Password
+
+
+        <label>Confirm Password</label>
           <input
             type="password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             required
           />
-        </label>
         {errors.confirmPassword && <p>{errors.confirmPassword}</p>}
+
+
+        <label>Profile Image</label>
+          <input
+            type="file"
+            value={profileImage}
+            onChange={(e) => setProfileImage(e.target.value)}
+            required
+          />
+        {errors.profileImage && <p>{errors.profileImage}</p>}
+
+
+        <label>Skin Type</label>
+          <input
+            type="text"
+            value={skinType}
+            onChange={(e) => setSkinType(e.target.value)}
+            required
+          />
+        {errors.skinType && <p>{errors.skinType}</p>}
+
         <button type="submit">Sign Up</button>
       </form>
-    </>
+    </div>
   );
 }
 
