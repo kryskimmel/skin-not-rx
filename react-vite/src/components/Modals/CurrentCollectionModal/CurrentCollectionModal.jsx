58f51767ef00cb587d8a3b401fd37b589fe28d82
Krystal Kimmel <kabkimmel@gmail.com>
2024-01-02 18:37:44 -0600

    refactored code

--- react-vite/src/components/Modals/CurrentCollectionModal/CurrentCollectionModal.jsx
+++ react-vite/src/components/Modals/CurrentCollectionModal/CurrentCollectionModal.jsx
@@ -1,45 +1,36 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import * as productActions from "../../../redux/product";
-// import { useModal } from '../../../context/Modal';
 import DeleteProductModal from '../DeleteProductModal';
 import OpenModalButton from '../OpenModalButton/OpenModalButton';
+// import { useModal } from '../../../context/Modal';
 import "./CurrentCollectionModal.css";
 
 
 
 
-function CurrentCollectionModal({collectionName}) {
-    const dispatch = useDispatch();
-    const user = useSelector(store => store.session.user);
-    const collectionProducts = useSelector(store => store.product.byProductType[collectionName]);
-    useEffect(() => { dispatch(productActions.getAllProducts())}, [dispatch]);
-
-    const userProducts = collectionProducts?.filter(product => product.user_id === user.id);
-
+function CurrentCollectionModal({premadeCollectionName, items}) {
     return (
-        <>
-        <div className='users-products-container'>
-            <h1 style={{display:"flex", justifyContent:"center"}}>{collectionName?.toUpperCase()}</h1>
-            {!userProducts?.length ? <h2 style={{display:"flex", justifyContent:"center"}}>You have not created any {collectionName}!</h2> : <h2 style={{display:"flex", justifyContent:"center"}}>({userProducts.length} {userProducts.length === 1 ? "Item" : "Items"})</h2>}
-            {userProducts?.map((product) =>
-                <div className='users-products-tile' key={product.id}>
-                    <img src={product.preview_image} alt={product.product_name} width={"200px"} height={"200px"} style={{objectFit:"cover", borderRadius:"15px"}}/>
-                    <ul style={{listStyle:"none", paddingLeft:"0px"}}>
-                        <li style={{fontWeight:"bold"}}>{product.brand_name}</li>
-                        <li>{product.product_name}</li>
-                    </ul>
-                    <div className='users-products-management-buttons'>
-                        <button>UPDATE</button>
-                        <OpenModalButton
-                            buttonText="DELETE"
-                            modalComponent={<DeleteProductModal productId={product.id} />}
-                        />
+        <div className='user-products-wrapper'>
+            <h1 className='user-products-h1'>{premadeCollectionName}</h1>
+            <div className='user-products-div'>
+                {items?.map((item) => (
+                    <div className='user-products-product-tile' key={item.id}>
+                        <img src={item.preview_image} alt={item.product_name} title={item.product_name} className="user-products-img" />
+                        <div className='user-products-info-div'>
+                            <ul>
+                                <li style={{fontWeight:'600'}}>{item.brand_name}</li>
+                                <li>{item.product_name}</li>
+                            </ul>
+                            <div className='user-products-management-buttons'>
+                                <button>UPDATE</button>
+                                <OpenModalButton
+                                    buttonText="DELETE"
+                                    modalComponent={<DeleteProductModal productId={item.id} />}
+                                />
+                            </div>
+                        </div>
                     </div>
-                </div>
-            )}
+                ))}
+            </div>
         </div>
-        </>
     )
 }
 
