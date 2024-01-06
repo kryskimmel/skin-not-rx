4973fbaf9a5a619e1df8cfc6af1f32b2b9f9212a
Krystal Kimmel <kabkimmel@gmail.com>
2024-01-05 19:36:18 -0600

    Created Navlinks to Products and Collections pages

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -20,7 +20,7 @@ function Navigation() {
         {user ?
             <>
               <li><NavLink to="/" className="home">HOME</NavLink></li>
-              <li><NavLink to='' className="products">PRODUCTS</NavLink></li>
+              <li><NavLink to='/users/current/products' className="products">PRODUCTS</NavLink></li>
               <li><NavLink to='' className="collections">COLLECTIONS</NavLink></li>
               <li className="create">
                 <OpenModalButton
