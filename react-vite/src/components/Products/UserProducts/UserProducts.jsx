ac2016c6e0831ef8aed515006aca00ec1c42414c
Krystal Kimmel <kabkimmel@gmail.com>
2024-01-05 19:36:42 -0600

    Added user products to component

--- react-vite/src/components/Products/UserProducts/UserProducts.jsx
+++ react-vite/src/components/Products/UserProducts/UserProducts.jsx
@@ -1,11 +1,62 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import * as ProductActions from "../../../redux/product";
+import OpenModalButton from "../../Modals/OpenModalButton/OpenModalButton";
+import ProductInfoModal from "../../Modals/ProductInfoModal";
 import "./UserProducts.css";
 
 
 function UserProducts() {
+    const dispatch = useDispatch();
+    const user = useSelector(state => state.session.user);
+    const userProducts = useSelector(state => state.product.allProducts);
+
+    useEffect(() => {
+        dispatch(ProductActions.viewCurrUserProducts())
+    }, [dispatch])
+
+
     return (
-        <>
-        User Products
-        </>
+        <div className="user-profile-products-wrapper">
+            <h1 className="user-profile-products-h1"> {`${user.username}'s Products`.toUpperCase()} </h1>
+            <div className="user-profile-products-div">
+                {userProducts
+                ? userProducts.map((attr) => (
+                <div key={attr.id}>
+                    <OpenModalButton
+                        className="user-profile-products-buttons"
+                        buttonText={
+                            <div className="user-profile-products-tile">
+                                <div className="user-profile-products-tile-img-div">
+                                    <img src={attr.preview_image} className="user-profile-products-tile-img"/>
+                                </div>
+                                <div className="user-profile-products-tile-info">
+                                    <ul className="user-profile-products-tile-ul">
+                                        <li style={{fontWeight:"600"}}>{attr.brand_name}</li>
+                                        <li>{attr.product_name}</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        }
+                        modalComponent={<ProductInfoModal productId={attr.id}/>}
+                    />
+                </div>
+
+                    // <div className="user-profile-products-tile" key={attr.id}>
+                    //     <div className="user-profile-products-tile-img-div">
+                    //         <img src={attr.preview_image} className="user-profile-products-tile-img"/>
+                    //     </div>
+                    //     <div className="user-profile-products-tile-info">
+                    //         <ul className="user-profile-products-tile-ul">
+                    //             <li style={{fontWeight:"600"}}>{attr.brand_name}</li>
+                    //             <li>{attr.product_name}</li>
+                    //         </ul>
+                    //     </div>
+                    // </div>
+                ))
+                : <h2>You have not created any products!</h2>}
+            </div>
+        </div>
     )
 
 }
