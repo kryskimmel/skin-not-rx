cbab3192963d22a114ab13f3b2b202625106e726
Krystal Kimmel <kabkimmel@gmail.com>
2024-01-05 19:35:35 -0600

    Added user management button tools such as update and delete

--- react-vite/src/components/Modals/ProductInfoModal/ProductInfoModal.jsx
+++ react-vite/src/components/Modals/ProductInfoModal/ProductInfoModal.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import * as productActions from "../../../redux/product";
+import OpenModalButton from "../OpenModalButton/OpenModalButton";
+import DeleteProductModal from "../DeleteProductModal";
 import { Icon } from '@iconify/react';
 import "./ProductInfoModal.css";
 
@@ -14,7 +16,6 @@ function ProductInfoModal( {productId}) {
         console.log('the product id', productId)
         dispatch(productActions.viewProductDetails(productId)).then(() => {setIsLoaded(true)})
     }, [productId])
-
 
 
     return isLoaded && (
@@ -32,6 +33,27 @@ function ProductInfoModal( {productId}) {
                     <li><span style={{fontWeight:"600"}}>SKIN CONCERN: </span> {product?.skin_concern}</li>
                     <li><span style={{fontWeight:"600"}}>PRODUCT LINK: </span><a href={product?.product_link} target="_blank" rel='noreferrer'><Icon icon="uil:link" width="20" height="20" /></a></li>
                 </ul>
+            </div>
+            <div className="product-info-tools-div">
+                <OpenModalButton
+                    title={'Close'}
+                    buttonText={<Icon icon="ph:x-square-bold" width="30" height="30" />}
+                />
+                <div className="product-info-mgmt-tools-div">
+                    <OpenModalButton
+                        title={'Edit'}
+                        buttonText={<Icon icon="bxs:edit" width="30" height="30"/>}
+                    />
+                    <OpenModalButton
+                        title={'Delete'}
+                        buttonText={<Icon icon="ph:trash-bold" width="30" height="30" />}
+                        modalComponent={<DeleteProductModal
+                            brandName={product.brand_name}
+                            productName={product.product_name}
+                            productId={product.id}
+                        />}
+                    />
+                </div>
             </div>
         </div>
     )
