baf4f7cc462aa7921378b9a09aac82689d1417ac
Krystal Kimmel <kabkimmel@gmail.com>
2024-01-07 15:45:08 -0600

    modified

--- app/models/product.py
+++ app/models/product.py
@@ -1,16 +1,7 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
-from sqlalchemy import Integer, String, Text, DateTime, Column, ForeignKey, Table, PrimaryKeyConstraint
+from sqlalchemy import Integer, String, Text, DateTime, Column, ForeignKey
 from datetime import datetime
 # from flask_login import UserMixin
-
-collection_product = Table(
-    "collection_product",
-    db.Model.metadata,
-    Column("collection_id", ForeignKey(add_prefix_for_prod("collections.id")), primary_key=True),
-    Column("product_id", ForeignKey(add_prefix_for_prod("products.id")), primary_key=True),
-    PrimaryKeyConstraint("collection_id", "product_id"),
-    extend_existing=True
-)
 
 
 class Product(db.Model):
