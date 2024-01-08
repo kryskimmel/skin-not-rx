2dcbaf1dfe8f221ce0a6b2e577c9fc4cf6887e6e
Krystal Kimmel <kabkimmel@gmail.com>
2024-01-07 21:28:38 -0600

    updated redux state for add collection

--- react-vite/src/redux/collection.js
+++ react-vite/src/redux/collection.js
@@ -149,14 +149,11 @@ export default function reducer(state = initialState, action){
         return newState;
       }
     case ADD_COLLECTION:
-      const updateAllCollections = [...state.allCollections, action.payload]
-      const updateById = {...state.byId, [action.payload.id] : action.payload}
-      newState = {
-        ...state,
-        allCollections: updateAllCollections,
-        byId: updateById
-      }
+      newState = JSON.parse(JSON.stringify(state));
+      newState.byId[`${action.payload.id}`] = action.payload
+      newState.allCollections = Object.values(newState.byId)
       return newState;
+
       case EDIT_COLLECTION:
         newState = JSON.parse(JSON.stringify(state));
         newState.byId[`${action.payload.id}`] = action.payload
