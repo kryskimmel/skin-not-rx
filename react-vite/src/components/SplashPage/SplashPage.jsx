6bed6b8a97a88dc94e7083fffd5d28bb2953c3cb
Krystal Kimmel <kabkimmel@gmail.com>
2024-01-26 23:54:22 -0600

    moved around code

--- react-vite/src/components/SplashPage/SplashPage.jsx
+++ react-vite/src/components/SplashPage/SplashPage.jsx
@@ -8,20 +8,25 @@ function SplashPage() {
 
     return (
         <div className="splashpage-container">
-            <div className="splashpage-left">
+            <div className="splashpage-content">
+                <div className="splashpage-heading">
+                    <h1 style={{ fontSize: '70px', }}>SKIN-NOT-RX</h1>
+                    {/* <h2>(Skin-r̶x̶)</h2> */}
+                </div>
+                {user && (
+                    <div className="splashpage-user-greeting-container">
+                        <h2 className="splashpage-greeting">Hello {user.username}!</h2>
+                        <img src={user.profile_image} alt={`${user.profile_image} profile image`} className="splashpage-profile-image" />
+                    </div>
+                )}
                 <div className="splashpage-header-container">
                     <h1 className="splashpage-header">Your personal skincare tracker</h1>
                     <p className="splashpage-subtext">Keeping track of what products you have, products you've loved,
                         and products that you wouldn't bother purchasing again shouldn't be hard...
                         and those are the conveniences that Skin-not-Rx aims to provide.</p>
                 </div>
+
             </div>
-            {user && (
-                <div className="splashpage-user-greeting-container">
-                    <h2 className="splashpage-greeting">Hello {user.username}!</h2>
-                    <img src={user.profile_image} alt={`${user.profile_image} profile image`} className="splashpage-profile-image" />
-                </div>
-            )}
         </div>
 
 
