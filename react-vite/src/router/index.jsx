526e7c77f426309ac91ec2e70110223446b00f95
Krystal <kabkimmel@gmail.com>
2024-02-20 09:43:46 -0600

    removed route to login form page

--- react-vite/src/router/index.jsx
+++ react-vite/src/router/index.jsx
@@ -1,9 +1,6 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from './Layout';
 import SplashPage from '../components/SplashPage/SplashPage';
-import Explore from '../components/Explore';
-import Profile from '../components/Profile';
-import Favorites from '../components/Favorites'
 import UserProducts from '../components/Products/UserProducts';
 import UserCollections from '../components/Collections/UserCollections';
 
@@ -24,18 +21,6 @@ export const router = createBrowserRouter([
         path: "/users/current/collections",
         element: <UserCollections />
       }
-      // {
-      //   path: "/explore",
-      //   element: <Explore />
-      // },
-      // {
-      //   path: "users/current/profile",
-      //   element: <Profile />,
-      // },
-      // {
-      //   path: "users/current/favorites",
-      //   element: <Favorites />
-      // },
     ],
   },
 ]);
