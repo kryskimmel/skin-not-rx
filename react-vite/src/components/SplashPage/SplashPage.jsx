63754beac9f32c4df367d6d2f78229fb55e4b4f3
Krystal <kabkimmel@gmail.com>
2024-02-20 09:53:00 -0600

    added conditional rendering for login button to display only when there is no user

--- react-vite/src/components/SplashPage/SplashPage.jsx
+++ react-vite/src/components/SplashPage/SplashPage.jsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
 import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
 import { motion as m } from 'framer-motion';
 import OpenModalButton from "../Modals/OpenModalButton/OpenModalButton";
 import LoginFormModal from "../Modals/LoginFormModal";
@@ -10,10 +8,7 @@ import "./SplashPage.css";
 
 function SplashPage() {
     const user = useSelector(state => state.session.user);
-    const [loginModalVisible, setLoginModalVisible] = useState(false)
-    const navigate = useNavigate();
 
-    console.log('is login visible?', loginModalVisible)
     return (
         <div className="splashpage-container">
             <m.div 
@@ -33,32 +28,16 @@ function SplashPage() {
                 and products that you wouldn't bother purchasing again shouldn't be hard...
                 and those are the conveniences that Skin-not-Rx aims to provide.
                 </p>
-                <OpenModalButton
-                    className="login-modal-button"
-                    buttonText="Log in"
-                    modalComponent={<LoginFormModal/>}
-                />
+                {!user && (
+                    <OpenModalButton
+                        className="login-modal-button"
+                        buttonText="Log in"
+                        modalComponent={<LoginFormModal/>}
+                    />
+                )}
             </m.div>
-            {/* <m.div 
-                initial={{ opacity: 0 }}
-                animate={{ opacity: 1 }}
-                // exit={{ opacity: 0 }}
-                onClick={() => navigate('/auth/login')}>
-                <iconify-icon 
-                    icon="ri:arrow-drop-down-line" 
-                    className="splashpage-dropdown-icon"
-                    style={{fontSize: '60px', color: 'white', position:'absolute', zindex:'2', cursor:'pointer'}}>
-                </iconify-icon>
-            </m.div> */}
-            {loginModalVisible && (
-                <m.div>
-                    <LoginFormModal isVisible={loginModalVisible} setIsVisible={setLoginModalVisible}/>     
-                </m.div>
-            )}
         </div>
     )
 };
-
-
 
 export default SplashPage;
