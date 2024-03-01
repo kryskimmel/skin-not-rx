1c9d8bf2a0e21a14ccbb9b16c244f4e3d9a51b95
Krystal <kabkimmel@gmail.com>
2024-03-01 10:57:26 -0600

    added logic to toggle navbar expansion

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -3,7 +3,6 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { thunkLogout } from "../../redux/session";
 import { NavLink } from "react-router-dom";
-// import ProfileButton from "./ProfileButton";
 import "./Navigation.css";
 import SearchBarAndFilter from "../NavigationPages/Search/SearchBarAndFIlter";
 import { Icon } from "@iconify/react";
@@ -16,6 +15,7 @@ function Navigation() {
   const [expandNav, setExpandNav] = useState(false);
   const [showSearch, setShowSearch] = useState(false);
   const searchRef = useRef();
+  const navRef = useRef();
 
   
   const handleNavExpansion = () => {
@@ -27,6 +27,20 @@ function Navigation() {
     setShowSearch(!showSearch);
     handleNavExpansion();
   };
+
+  const handleClickOutsideNav = (e) => {
+    if (navRef.current && !navRef.current.contains(e.target)) {
+      setExpandNav(false)
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener("mousedown", handleClickOutsideNav);
+
+    return () => {
+        document.removeEventListener("mousedown", handleClickOutsideNav);
+    };
+}, []);
 
   const logout = (e) => {
     e.preventDefault();
@@ -58,18 +72,26 @@ function Navigation() {
         </ul>
       </div>
       ) : (
-        <div className="nav-container-after">
+        <div className="nav-container-after" ref={navRef}>
           <ul className="nav-after-focus">
               <div className='nav-top-after'>
                 <li onClick={handleNavExpansion}><Icon icon="ion:caret-back" width={45}/></li>
                 <li><h2>{user.username}</h2></li>
-                <NavLink to={'/'}><img src={user.profile_image} alt="profile-img" width={125}/></NavLink>
+                <NavLink to={'/'} onClick={handleNavExpansion}>
+                  <img src={user.profile_image} alt="profile-img" width={125}/>
+                </NavLink>
             </div>
             <div className="nav-center-after">
               <li className='nav-item' style={{margin:'0px'}} onClick={handleSearch}><Icon icon="fluent:search-20-regular" width={35}/>SEARCH</li>
-              <NavLink to={'/users/current/products'} className='nav-item'><Icon icon="fluent:square-20-regular" width={35}/>PRODUCTS</NavLink>
-              <NavLink to={'/users/current/collections'} className='nav-item'><Icon icon="fluent:squares-nested-20-regular" width={35}/>COLLECTIONS</NavLink>
-              <NavLink className='nav-item'><Icon icon="fluent:heart-20-regular" width={35}/>FAVORITES</NavLink>
+                <NavLink to={'/users/current/products'} className='nav-item' onClick={handleNavExpansion}>
+                  <Icon icon="fluent:square-20-regular" width={35}/>PRODUCTS
+                </NavLink>
+              <NavLink to={'/users/current/collections'} className='nav-item' onClick={handleNavExpansion}>
+                <Icon icon="fluent:squares-nested-20-regular" width={35}/>COLLECTIONS
+              </NavLink>
+              <NavLink className='nav-item' onClick={handleNavExpansion}>
+                <Icon icon="fluent:heart-20-regular" width={35}/>FAVORITES
+              </NavLink>
             </div>
             <div className="nav-bottom-after">
               <button className="logout-button" onClick={logout}>LOG OUT</button>
