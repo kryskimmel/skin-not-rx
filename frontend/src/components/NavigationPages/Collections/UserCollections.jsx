8588db68c8aa19d59472b4fcb540be5008b9e88f
Krystal <kabkimmel@gmail.com>
2024-03-01 13:09:20 -0600

    moved divs within component

--- frontend/src/components/NavigationPages/Collections/UserCollections.jsx
+++ frontend/src/components/NavigationPages/Collections/UserCollections.jsx
@@ -5,7 +5,6 @@ import CurrentCollectionModal from "../../Modals/CollectionModals/CurrentCollect
 import CreateCollectionModal from "../../Modals/CollectionModals/CreateCollectionModal";
 import * as productActions from "../../../redux/product";
 import * as collectionActions from "../../../redux/collection";
-import { Icon } from '@iconify/react';
 import "./UserCollections.css";
 
 function UserCollections() {
@@ -22,28 +21,29 @@ function UserCollections() {
 
     return (
         <div className="collection-page-container">
-            <h1 className="collection-page-h1">My Custom Collections</h1>
-            <div className="collection-page-custom-collections-div">
-                <div className="create-custom-collection-div">
+             <div className="collections-header">
+                <h1 className="collection-page-h1">COLLECTIONS</h1>
                     <OpenModalButton
                         buttonText={
                             <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
                                 <p>Create A Collection</p>
-                                <Icon icon="basil:add-outline" width="30" height="30" />
                             </div>}
+                            className="collection-create-button"
                         modalComponent={<CreateCollectionModal />}
                     />
-                </div>
+            </div>
+            <div className="collection-page-custom-collections-div">
+           
                 {userCollections ? userCollections.map((collection) => (
                     <div key={collection.id}>
                         <OpenModalButton
                             buttonText={
                                 <div className="collection-page-collections-tile-div">
-                                    <h2 className="collection-page-h2">{collection.name} <span style={{ color: "#4D4B4B", fontSize: "16px" }}>({collection.Products?.length})</span></h2>
+                                    <h2 className="collection-page-h2">{collection.name}</h2>
                                     <div className="collection-page-collections-grid">
                                         {collection.Products?.slice(0, 4)?.map((attr, idx) => (
                                             <div className="collection-page-grid-images" key={idx}>
-                                                <img src={attr.preview_image[0].image_url} alt={attr.product_name} title={attr.product_name} width="100px" height="100px" />
+                                                <img src={attr.preview_image[0].image_url} alt={attr.product_name} title={attr.product_name} className="collection-image"/>
                                             </div>
                                         ))}
                                     </div>
