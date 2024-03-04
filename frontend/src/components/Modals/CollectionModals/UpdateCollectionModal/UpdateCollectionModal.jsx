a6e7373d2cc534e97f16efcec8a8746dfdbca59b
Krystal <kabkimmel@gmail.com>
2024-03-03 22:07:20 -0600

    refactored logic and removed divs that displayed products to add as these divs were added to the search component

--- frontend/src/components/Modals/CollectionModals/UpdateCollectionModal/UpdateCollectionModal.jsx
+++ frontend/src/components/Modals/CollectionModals/UpdateCollectionModal/UpdateCollectionModal.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable no-unused-vars */
 import { useDispatch, useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 import { useModal } from "../../../../context/Modal";
 import { modifyCollection } from "../../../../redux/collection";
-import SearchBarAndAddProduct from "../../../../utils/SearchBarAndAddProduct";
+import SearchBarAndAddProduct from "../SearchBarAndAddProduct";
 import { Icon } from '@iconify/react';
 import "./UpdateCollectionModal.css";
 
@@ -14,15 +15,13 @@ function UpdateCollectionModal ({collectionId, collectionName, items}) {
     const collectionToEdit = useSelector(state => state.collection.byId[collectionId])
     const [name, setName] = useState(collectionName);
     const [prevStoredProducts, setPrevStoredProducts] = useState(items);
-    const [newProductsToAdd, setNewProductsToAdd] = useState([]);
+    const [addProducts, setAddProducts] = useState([]);
     const [frontendErrors, setFrontendErrors] = useState({});
-    // eslint-disable-next-line no-unused-vars
     const [backendErrors, setBackendErrors] = useState({});
     const [showErrors, setShowErrors] = useState(false);
     const [submittedForm, setSubmittedForm] = useState(false);
     const [isDisabled, setIsDisabled] = useState(true);
-    // eslint-disable-next-line no-unused-vars
-    const validationErrors = {};
+    const submitButtonCN = isDisabled ? "disabled-collection-update-button" : "enabled-collection-update-button"
 
 
     useEffect(() => {
@@ -30,62 +29,15 @@ function UpdateCollectionModal ({collectionId, collectionName, items}) {
             setName(collectionToEdit.name || "")
             setPrevStoredProducts(collectionToEdit.Products)
         }
-    }, [collectionToEdit])
+    }, [collectionToEdit]);
 
-    console.log('Items:--', items)
-    console.log("SELECTED COLLECTION TO EDIT:--", collectionToEdit)
-    console.log("PREVIOUSLY STORED PRODUCTS:--", prevStoredProducts)
-
-    // To collect the data from the SearchBarAndAddProduct component
-    const handleProductsToAdd = (data) => {
-        console.log('*********DATA*****', data)
-        const noDuplicates = data.filter(product => !prevStoredProducts.some(existingProduct => existingProduct.id === product.id));
-
-        setNewProductsToAdd(noDuplicates);
-        setPrevStoredProducts([...noDuplicates, ...prevStoredProducts]);
-        console.log({ noDuplicates, prevStoredProducts });
-    }
-
-
-    // Grab product ids from the previously stored products
-    const prevStoredProductIds = [];
-    if (prevStoredProducts) {
-        console.log(true)
-        prevStoredProducts.map((attr) => {prevStoredProductIds.push(attr.id)})
-    }
-
-
-    // Grab product ids from new products to add
-    const productIdsToAdd = [];
-    if (newProductsToAdd) {
-        newProductsToAdd.map((attr) => {productIdsToAdd.push(attr.id)})
-    }
-
-
-    // Toggle submit button classname
-    const submitButtonCN = isDisabled ? "disabled-collection-update-button" : "enabled-collection-update-button"
-
-
-    // Function to remove a previously stored product from the collection
-    const removeProduct = (productId) => {
-        let newProductList = prevStoredProducts.filter(product => product.id !== productId);
-        setPrevStoredProducts(newProductList);
-        console.log('new list!!!',newProductList)
-    }
-
-
-    // useEffect to that will set IsDisabled status to true if required fields are not empty
+   
     useEffect(() => {
-        if (name && prevStoredProductIds.length > 0) {
-            setIsDisabled(false)
-        }
-        else {
-            setIsDisabled(true)
-        }
-    }, [name, prevStoredProductIds.length])
+        if (name && (prevStoredProducts.length > 0 || addProducts.length > 0)) setIsDisabled(false);
+        else setIsDisabled(true);
+    }, [name, prevStoredProducts.length, addProducts.length]);
 
 
-    // useEffect to keep track of validation errors
     useEffect(() => {
         const validationErrors = {};
         const inputRequired = "Input is required."
@@ -99,25 +51,18 @@ function UpdateCollectionModal ({collectionId, collectionName, items}) {
         else if (name.length < 3) validationErrors.name = minChar3;
 
         setFrontendErrors(validationErrors);
-    }, [dispatch, name, prevStoredProducts, newProductsToAdd]);
+    }, [dispatch, name, prevStoredProducts, addProducts]);
 
     useEffect(() => {
         setShowErrors(Object.values(frontendErrors).length > 0);
     }, [frontendErrors]);
 
 
-    console.log('BEFORE form submission', {
-        'name': name,
-        'user_id': currentUserId,
-        'product_ids': prevStoredProductIds
-    })
-
-
     // handle form submission
     const handleSubmit = async (e) => {
         e.preventDefault()
         setSubmittedForm(true)
-        const prodIds = prevStoredProducts.map((prod) => prod.id)
+        const prodIds = [...prevStoredProducts.map((prod) => prod.id), ...addProducts.map((prod) => prod.id)];
         const updatedCollection = {
             'name': name.trimEnd(),
             'user_id': currentUserId,
@@ -163,18 +108,7 @@ function UpdateCollectionModal ({collectionId, collectionName, items}) {
                     onChange={(e) => setName((e.target.value).trimStart())}
                 />
                 {showErrors && submittedForm && frontendErrors?.name && <p className="errors-text">{frontendErrors.name}</p>}
-                <SearchBarAndAddProduct productsToAdd={handleProductsToAdd}/>
-                <p style={{marginTop:'10px'}}>Products currently in colection:</p>
-                <div className="previously-stored-products-div">
-                    <ul className="previously-stored-products-ul">
-                    {Array.isArray(prevStoredProducts) && prevStoredProducts.map((product) => (
-                        <li className="previously-stored-products-item" key={product.id}>
-                            {product.brand_name}: {product.product_name}
-                            <p className="remove-prod" onClick={() => removeProduct(product.id)}>Remove</p>
-                        </li>
-                    ))}
-                    </ul>
-                </div>
+                <SearchBarAndAddProduct setAddProducts={setAddProducts} prevStoredProducts={prevStoredProducts} setPrevStoredProducts={setPrevStoredProducts}/>
                 <div className="update-collection-button-div">
                     <button type="submit" className={submitButtonCN} disabled={isDisabled}>Update</button>
                 </div>
