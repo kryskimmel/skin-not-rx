415b7e70644dacbdeccb5297b830cb292dab9a10
Krystal <kabkimmel@gmail.com>
2024-03-04 09:50:32 -0600

    created seed file for favorite collections

---
+++ backend/app/seeds/favorite_collections.py
@@ -0,0 +1,21 @@
+from app.models import db, Favorite_Collection, environment, SCHEMA
+from sqlalchemy.sql import text
+from .seed_data.favorite_collections_data import favorite_collections_data
+
+def seed_products():
+    for favorite_collection in favorite_collections_data:
+        seed_favorite_collection = Favorite_Collection(
+            user_id=favorite_collection['user_id'],
+            collection_id=favorite_collection['collection_id']
+        )
+        db.session.add(seed_favorite_collection)
+    db.session.commit()
+
+
+def undo_products():
+    if environment == "production":
+        db.session.execute(f"TRUNCATE table {SCHEMA}.products RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute(text("DELETE FROM favorite_collections"))
+
+    db.session.commit()
