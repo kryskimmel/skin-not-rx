6354af525d23247127c09f1ebcadfc4146e25393
Krystal <kabkimmel@gmail.com>
2024-03-04 13:11:38 -0600

    Imported FavoriteProducts and FavoriteCollections components

--- frontend/src/components/NavigationPages/Favorites/Favorites.jsx
+++ frontend/src/components/NavigationPages/Favorites/Favorites.jsx
@@ -1,9 +1,12 @@
 import FavoriteProducts from "./FavoriteProducts";
+import FavoriteCollections from "./FavoriteCollections";
+import './Favorites.css'
 
 function Favorites () {
     return (
         <div className="favorites-container">
             <FavoriteProducts/>
+            <FavoriteCollections/>
         </div>
     )
 }
