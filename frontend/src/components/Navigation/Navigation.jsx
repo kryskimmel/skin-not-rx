46680ed6a341fb346cfbcfb7340a6a1900aa6f51
Krystal <kabkimmel@gmail.com>
2024-03-05 14:53:29 -0600

    added div that has a background color to it for when the navigation is expanded

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -60,7 +60,7 @@ function Navigation() {
 
 
   return (
-    <>
+    <div>
     {showSearch && (<SearchBarAndFilter showSearch={showSearch} searchRef={searchRef} />)}
     {!expandNav ? (
         <div className="nav-container-before">
@@ -71,34 +71,36 @@ function Navigation() {
         </ul>
       </div>
       ) : (
-        <div className="nav-container-after" ref={navRef}>
-          <ul className="nav-after-focus">
-              <div className='nav-top-after'>
-                <li onClick={handleNavExpansion}><Icon icon="ion:caret-back" width={45}/></li>
-                <li><h2>{user.username}</h2></li>
-                <NavLink to={'/'} onClick={handleNavExpansion}>
-                  <img src={user.profile_image} alt="profile-img" width={125}/>
+        <div className="nav-open-background">
+          <div className="nav-container-after" ref={navRef}>
+            <ul className="nav-after-focus">
+                <div className='nav-top-after'>
+                  <li onClick={handleNavExpansion}><Icon icon="ion:caret-back" width={45}/></li>
+                  <li><h2>{user.username}</h2></li>
+                  <NavLink to={'/'} onClick={handleNavExpansion}>
+                    <img src={user.profile_image} alt="profile-img" width={125}/>
+                  </NavLink>
+              </div>
+              <div className="nav-center-after">
+                <li className='nav-item' style={{margin:'0px'}} onClick={handleSearch}><Icon icon="fluent:search-20-regular" width={35}/>SEARCH</li>
+                  <NavLink to={'/users/current/products'} className='nav-item' onClick={handleNavExpansion}>
+                    <Icon icon="fluent:square-20-regular" width={35}/>PRODUCTS
+                  </NavLink>
+                <NavLink to={'/users/current/collections'} className='nav-item' onClick={handleNavExpansion}>
+                  <Icon icon="fluent:squares-nested-20-regular" width={35}/>COLLECTIONS
                 </NavLink>
-            </div>
-            <div className="nav-center-after">
-              <li className='nav-item' style={{margin:'0px'}} onClick={handleSearch}><Icon icon="fluent:search-20-regular" width={35}/>SEARCH</li>
-                <NavLink to={'/users/current/products'} className='nav-item' onClick={handleNavExpansion}>
-                  <Icon icon="fluent:square-20-regular" width={35}/>PRODUCTS
+                <NavLink to={'/users/current/favorites'} className='nav-item' onClick={handleNavExpansion}>
+                  <Icon icon="fluent:heart-20-regular" width={35}/>FAVORITES
                 </NavLink>
-              <NavLink to={'/users/current/collections'} className='nav-item' onClick={handleNavExpansion}>
-                <Icon icon="fluent:squares-nested-20-regular" width={35}/>COLLECTIONS
-              </NavLink>
-              <NavLink to={'/users/current/favorites'} className='nav-item' onClick={handleNavExpansion}>
-                <Icon icon="fluent:heart-20-regular" width={35}/>FAVORITES
-              </NavLink>
-            </div>
-            <div className="nav-bottom-after">
-              <button className="logout-button" onClick={logout}>LOG OUT</button>
-            </div>
-          </ul>
+              </div>
+              <div className="nav-bottom-after">
+                <button className="logout-button" onClick={logout}>LOG OUT</button>
+              </div>
+            </ul>
+          </div>
         </div>
       )}
-    </>
+    </div>
   )
 }
 
