c57b8126892fb05c4d5a9a1e2f073f90fcc8d251
Krystal <kabkimmel@gmail.com>
2024-03-06 12:29:07 -0600

    added optional chaining and conditional rendering

--- frontend/src/components/NavigationPages/Favorites/FavoriteProducts.jsx
+++ frontend/src/components/NavigationPages/Favorites/FavoriteProducts.jsx
@@ -36,13 +36,15 @@ function FavoriteProducts() {
 
     const faveProductList = useMemo(() => {
         const faveProductList = [];
-        for (let faveProduct in favoriteProducts) {
-            faveProductList.push({
-                id: favoriteProducts[faveProduct].product_details.id,
-                brand_name: favoriteProducts[faveProduct].product_details.brand_name,
-                product_name: favoriteProducts[faveProduct].product_details.product_name,
-                preview_image: favoriteProducts[faveProduct].product_details.preview_image,
-            });
+        if (favoriteProducts) {
+            for (let faveProduct in favoriteProducts) {
+                faveProductList.push({
+                    id: favoriteProducts[faveProduct].product_details?.id,
+                    brand_name: favoriteProducts[faveProduct].product_details?.brand_name,
+                    product_name: favoriteProducts[faveProduct].product_details?.product_name,
+                    preview_image: favoriteProducts[faveProduct].product_details?.preview_image,
+                });
+            }
         }
         return faveProductList;
     }, [favoriteProducts]);
@@ -57,8 +59,8 @@ function FavoriteProducts() {
     useEffect(() => {
         faveProductList.filter(
             (faveProd) => (
-                faveProd.product_name.toLowerCase() === searchInput.toLowerCase() ||
-                faveProd.brand_name.toLowerCase() === searchInput.toLowerCase()) &&
+                faveProd.product_name?.toLowerCase() === searchInput.toLowerCase() ||
+                faveProd.brand_name?.toLowerCase() === searchInput.toLowerCase()) &&
                 setProductId(faveProd.id)
         );
         setShowOptions(true);
@@ -97,94 +99,98 @@ function FavoriteProducts() {
                     onChange={handleInputChange}
                 />
             </div>
-            <div 
-                className="fave-search-options"
-                style={{display: showOptions ? "" : "none"}}
-            >
-            {faveProductList
-                .filter((faveProd) => {
-                    const searchTerm = searchInput.toLowerCase();
-                    const productName = faveProd.product_name.toLowerCase();
-                    const productBrandName = faveProd.brand_name.toLowerCase();
-                    return searchTerm && (productName.startsWith(searchTerm) || productBrandName.startsWith(searchTerm));
-                })
-                .map((faveProduct) =>(
-                    <div key={`search-faveProd-${faveProduct.id}=${faveProduct.product_name}`}>
-                        <OpenModalButton
-                            className="fave-search-options-tile-btn"
-                            buttonText={
-                                <>
-                                    <img 
-                                        src={faveProduct.preview_image}
-                                        className="fave-search-result-img"  
-                                    />
-                                    <div className="fave-search-prod-star-div" onClick={(e) => { 
-                                        e.stopPropagation(); 
-                                        handleRemoveFavorite(faveProduct.id); 
-                                        removeFavoriteLocalStorage(faveProduct.product_id) 
-                                    }}>
-                                        <Icon 
-                                            icon='fluent:star-20-filled' 
-                                            width={25} 
-                                            height={25} 
-                                            color="#9cb781" 
-                                            className="star-icon"/>
-                                    </div>
-                                    <div>
-                                        <ul className="fave-prod-tile-info-ul">
-                                            <li style={{ fontWeight: "600" }}>{faveProduct.brand_name}</li>
-                                            <li>{faveProduct.product_name}</li>
-                                        </ul>
-                                    </div>
-                                </>
-                            }
-                            modalComponent={<ProductInfoModal productId={faveProduct.id} />}
-                            // onButtonClick={() => { setSearchInput("") ;}}
-                        />
-                    </div>
-                ))
-            }
-            </div>
-            {favoriteProducts.length ? (
-                <div className="fave-prod-tiles-div">
-                    {favoriteProducts.map((faveProd) => (
-                        <OpenModalButton
-                            key={`${faveProd.id}-faveProd-${faveProd.product_id}`}
-                            className="fave-prod-tile-btn"
-                            buttonText={
-                                <>
-                                    <img 
-                                        src={faveProd.product_details?.preview_image} 
-                                        className="fave-prod-tile-img" 
-                                    />
-                                    <div className="fave-prod-star-div" onClick={(e) => { 
-                                        e.stopPropagation(); 
-                                        handleRemoveFavorite(faveProd.id); 
-                                        removeFavoriteLocalStorage(faveProd.product_id) 
-                                    }}>
-                                        <Icon 
-                                            icon='fluent:star-20-filled' 
-                                            width={25} 
-                                            height={25} 
-                                            color="#9cb781" 
-                                            className="star-icon"/>
-                                    </div>
-                                    <div>
-                                        <ul className="fave-prod-tile-info-ul">
-                                            <li style={{ fontWeight: "600" }}>{faveProd.product_details?.brand_name}</li>
-                                            <li>{faveProd.product_details?.product_name}</li>
-                                        </ul>
-                                    </div>
-                                </>
-                            }
-                            modalComponent={<ProductInfoModal productId={faveProd.product_id} />}
-                        />
-                    ))}
-                </div>
-            ) : (
-                <div className="no-fave-prod-div">
-                    <h3 className="no-fave-prod-text">You have not favorited any products yet!</h3>
-                </div>
+            {searchInput ? (
+                <div 
+                    className="fave-search-options"
+                    style={{display: showOptions ? "" : "none"}}
+                >
+                    {faveProductList
+                        .filter((faveProd) => {
+                            const searchTerm = searchInput.toLowerCase();
+                            const productName = faveProd.product_name.toLowerCase();
+                            const productBrandName = faveProd.brand_name.toLowerCase();
+                            return searchTerm && (productName.startsWith(searchTerm) || productBrandName.startsWith(searchTerm));
+                        })
+                        .map((faveProduct) =>(
+                            <div key={`search-faveProd-${faveProduct.id}=${faveProduct.product_name}`}>
+                                <OpenModalButton
+                                    className="fave-search-options-tile-btn"
+                                    buttonText={
+                                        <>
+                                            <img 
+                                                src={faveProduct.preview_image}
+                                                className="fave-search-result-img"  
+                                            />
+                                            <div className="fave-search-prod-star-div" onClick={(e) => { 
+                                                e.stopPropagation(); 
+                                                handleRemoveFavorite(faveProduct.id); 
+                                                removeFavoriteLocalStorage(faveProduct.product_id) 
+                                            }}>
+                                                <Icon 
+                                                    icon='fluent:star-20-filled' 
+                                                    width={25} 
+                                                    height={25} 
+                                                    color="#9cb781" 
+                                                    className="star-icon"/>
+                                            </div>
+                                            <div>
+                                                <ul className="fave-prod-tile-info-ul">
+                                                    <li style={{ fontWeight: "600" }}>{faveProduct.brand_name}</li>
+                                                    <li>{faveProduct.product_name}</li>
+                                                </ul>
+                                            </div>
+                                        </>
+                                    }
+                                    modalComponent={<ProductInfoModal productId={faveProduct.id} />}
+                                    // onButtonClick={() => { setSearchInput("") ;}}
+                                />
+                            </div>
+                        ))
+                    }
+                </div>) : (
+                <>
+                    {favoriteProducts.length ? (
+                        <div className="fave-prod-tiles-div">
+                            {favoriteProducts.map((faveProd) => (
+                                <OpenModalButton
+                                    key={`${faveProd.id}-faveProd-${faveProd.product_id}`}
+                                    className="fave-prod-tile-btn"
+                                    buttonText={
+                                        <>
+                                            <img 
+                                                src={faveProd.product_details?.preview_image} 
+                                                className="fave-prod-tile-img" 
+                                            />
+                                            <div className="fave-prod-star-div" onClick={(e) => { 
+                                                e.stopPropagation(); 
+                                                handleRemoveFavorite(faveProd.id); 
+                                                removeFavoriteLocalStorage(faveProd.product_id) 
+                                            }}>
+                                                <Icon 
+                                                    icon='fluent:star-20-filled' 
+                                                    width={25} 
+                                                    height={25} 
+                                                    color="#9cb781" 
+                                                    className="star-icon"/>
+                                            </div>
+                                            <div>
+                                                <ul className="fave-prod-tile-info-ul">
+                                                    <li style={{ fontWeight: "600" }}>{faveProd.product_details?.brand_name}</li>
+                                                    <li>{faveProd.product_details?.product_name}</li>
+                                                </ul>
+                                            </div>
+                                        </>
+                                    }
+                                    modalComponent={<ProductInfoModal productId={faveProd.product_id} />}
+                                />
+                            ))}
+                        </div>
+                    ) : (
+                        <div className="no-fave-prod-div">
+                            <h3 className="no-fave-prod-text">You have not favorited any products yet!</h3>
+                        </div>
+                    )}  
+                </>
             )}
         </div>
     );
