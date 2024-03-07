c4f22543d69e76fd57570732e7a9e6916ee05d35
Krystal <kabkimmel@gmail.com>
2024-03-06 18:09:32 -0600

    added link to homepage, project github repo, and personal github profile

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,8 +1,30 @@
+import { useNavigate } from "react-router-dom";
+import { Icon } from "@iconify/react";
 import "./Footer.css";
 
 function Footer () {
+    const navigate = useNavigate();
+
     return (
-        <p>this is the footer</p>
+        <div className="footer-container">
+            <div className="footer-left">
+                <div className="footer-skinnotrx">
+                    <p className="skinnotrx-text" onClick={() => navigate("/")}>SKIN-NOT-R̶X̶</p>
+                </div>
+            </div>
+            <div className="footer-right">
+                <div className="footer-github-link">
+                    <a href="https://github.com/kryskimmel/skin-not-rx" target="_blank" rel="noreferrer">
+                        <Icon icon="fa:github" color="#000000" width={30} height={30}/>
+                    </a>
+                </div>
+                <div className="footer-personal-github-link">
+                    <a href="https://github.com/kryskimmel" target="_blank" rel="noreferrer" className="personal-github-link">
+                        <p>Krystal&apos;s Github</p>
+                    </a>
+                </div>
+            </div>  
+        </div>
     )
 }
 
