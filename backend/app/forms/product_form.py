14af5da625a0667d8defcdd7f8b79163906bcfa8
Krystal <kabkimmel@gmail.com>
2024-03-08 16:31:50 -0600

    changed character length field input requirements

--- backend/app/forms/product_form.py
+++ backend/app/forms/product_form.py
@@ -5,24 +5,24 @@ from flask_wtf.file import FileField, FileAllowed, FileRequired
 from app.awsS3 import ALLOWED_EXTENSIONS
 from app.models import Product
 
-def character_max_60(form, field):
+def char_max60(form, field):
     if len(field.data) > 60:
         raise ValidationError("Input must not exceed 60 characters.")
 
-def character_max_300(form, field):
-    if len(field.data) > 300:
-        raise ValidationError("Input must not exceed 300 characters.")
-
-def character_max_500(form, field):
+def char_max500(form, field):
     if len(field.data) > 500:
         raise ValidationError("Input must not exceed 500 characters.")
 
-def character_min_3(form, field):
-    if field.data and len(field.data) < 3:
-        raise ValidationError("Input must be at least 3 characters long.")
+def char_min2(form, field):
+    if len(field.data) < 2:
+        raise ValidationError("Input must be at least 2 characters long.")
+    
+def char_min4(form, field):
+    if len(field.data) < 4:
+        raise ValidationError("Input must be at least 4 characters long.")
 
-def starting_with_spaces(form, field):
-    if field.data and (field.data).startswith(' '):
+def beginning_spaces(form, field):
+    if field.data.startswith(' '):
         raise ValidationError('Input cannot begin with a space.')
 
 
@@ -33,10 +33,52 @@ def product_name_exists(form, field):
         raise ValidationError('This product already exists. Please try adding a different product.')
 
 class ProductForm(FlaskForm):
-    brand_name = StringField('brand_name', validators=[DataRequired(), character_max_60, starting_with_spaces])
-    product_name = StringField('product_name', validators=[DataRequired(), character_max_60, starting_with_spaces])
-    product_type = StringField('product_type', validators=[DataRequired(), character_max_60, starting_with_spaces])
-    description = StringField('description', validators=[DataRequired(), character_max_500, starting_with_spaces])
-    key_ingredients = StringField('key_ingredients', validators=[character_max_500, character_min_3, starting_with_spaces])
-    product_link = StringField('product_link', validators=[character_max_500, character_min_3, starting_with_spaces])
-    image_url = FileField('image_url', validators=[FileRequired(), FileAllowed(ALLOWED_EXTENSIONS)])
+    brand_name = StringField(
+        'brand_name', 
+        validators=[
+            DataRequired(), 
+            char_min2,
+            char_max60,
+            beginning_spaces
+        ])
+    product_name = StringField(
+        'product_name', 
+        validators=[
+            DataRequired(), 
+            char_min2,
+            char_max60,
+            beginning_spaces
+        ])
+    product_type = StringField(
+        'product_type', 
+        validators=[
+            DataRequired(),
+        ])
+    description = StringField(
+        'description', 
+        validators=[
+            DataRequired(),
+            char_min4,
+            char_max500,
+            beginning_spaces
+        ])
+    key_ingredients = StringField(
+        'key_ingredients', 
+        validators=[
+            char_min2,
+            char_max500,
+            beginning_spaces
+        ])
+    product_link = StringField(
+        'product_link', 
+        validators=[
+            char_min4,
+            char_max500,
+            beginning_spaces
+        ])
+    image_url = FileField(
+        'image_url', 
+        validators=[
+            FileRequired(), 
+            FileAllowed(ALLOWED_EXTENSIONS)
+        ])
