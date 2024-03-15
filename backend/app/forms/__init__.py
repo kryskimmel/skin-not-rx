b9c5c43c19a01fbddd07d8023f84c059fca2dbed
Krystal <kabkimmel@gmail.com>
2024-03-15 16:21:31 -0500

    imported UpdateProductForm

--- backend/app/forms/__init__.py
+++ backend/app/forms/__init__.py
@@ -2,3 +2,4 @@ from .login_form import LoginForm
 from .signup_form import SignUpForm
 from .product_form import ProductForm
 from .collection_form import CollectionForm
+from .update_product_form import UpdateProductForm
