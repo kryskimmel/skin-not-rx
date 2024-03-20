e765a37a3417c1b87f33b457b8a91d51e753d21c
Krystal <kabkimmel@gmail.com>
2024-03-20 17:59:02 -0500

    changed import statements to import models from .models and tables from .tables

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,7 +1,3 @@
 from .db import db, environment, SCHEMA
-from .user import User
-from .product import Product
-from .collection import Collection
-from .collection_product import collection_product
-from .favorite_product import Favorite_Product
-from .favorite_collection import Favorite_Collection
+from .models import User, Product, Collection, Favorite_Product, Favorite_Collection
+from .tables import product_collections, favorited_products, favorited_collections
