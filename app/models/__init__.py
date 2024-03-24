42965c2b7c0c386e14273de93c4c3b63720a9cb1
Krystal <kabkimmel@gmail.com>
2024-03-24 16:15:41 -0500

    changed imports

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,2 @@
 from .db import db, environment, SCHEMA
 from .models import User, Product, Collection, Favorite_Product, Favorite_Collection
-from .tables import product_collections, favorited_products, favorited_collections
