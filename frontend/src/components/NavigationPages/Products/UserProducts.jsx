b5a4518163c280f9a4163675bf91bddc2e8923bf
Krystal <kabkimmel@gmail.com>
2024-03-24 16:34:02 -0500

    removed star div

--- frontend/src/components/NavigationPages/Products/UserProducts.jsx
+++ frontend/src/components/NavigationPages/Products/UserProducts.jsx
@@ -1,48 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import { getCurrUserProducts } from "../../../redux/product";
-import { addProductToFavorites } from "../../../redux/favoriteProduct";
 import OpenModalButton from "../../../utils/OpenModalButton";
 import SearchProductsModal from "../../Modals/ProductModals/SearchProductsModal";
 import ProductInfoModal from "../../Modals/ProductModals/ProductInfoModal";
 import CreateProductModal from "../../Modals/ProductModals/CreateProductModal";
-import Collapsible from "../../../utils/Collapsible";
 import { Icon } from "@iconify/react";
 import "./UserProducts.css";
 
 function UserProducts() {
     const dispatch = useDispatch();
     const userProducts = useSelector(state => state.products.allProducts);
-    const userProductsByType = useSelector(state => state.products.byProductType);
-    const [isFavorite, setIsFavorite] = useState(() => {
-        const storedFavorites = localStorage.getItem('favoriteProducts');
-        return storedFavorites ? JSON.parse(storedFavorites) : {};
-    });
-    const [openCollapsible, setOpenCollapsible] = useState(null);
 
-    const handleCollapsibleClick = (productType) => {
-      setOpenCollapsible(productType === openCollapsible ? null : productType);
-    };
- 
     useEffect(() => {
         dispatch(getCurrUserProducts())
     }, [dispatch]);
-
-    const handleFavoriteClick = (prodId) => {
-        setIsFavorite((prev) => {
-            const updatedFavorites = {
-                ...prev,
-                [prodId]: true
-            };
-            localStorage.setItem('favoriteProducts', JSON.stringify(updatedFavorites));
-
-            if (!prev[prodId]) {
-                dispatch(addProductToFavorites({ product_id: prodId }));
-            }
-            return updatedFavorites;
-        });
-    };
-
 
     return (
         <div className="prod-page-container">
@@ -77,20 +49,6 @@ function UserProducts() {
                                             src={attr.preview_image} 
                                             className="prod-tile-img"
                                         />
-                                        <div className="prod-star-div" onClick={(e) => { 
-                                            e.stopPropagation(); 
-                                            handleFavoriteClick(attr.id); 
-                                        }}>
-                                            {isFavorite[attr.id] ? 
-                                            <p className="fave-prod-text">favorite</p> : 
-                                            <Icon 
-                                                icon='fluent:star-20-regular' 
-                                                width={25} 
-                                                height={25} 
-                                                className="star-icon"
-                                            />  
-                                            }
-                                        </div>
                                         <div>
                                             <ul className="prod-tile-info-ul">
                                                 <li style={{ fontWeight: "600" }}>{attr.brand_name}</li>
@@ -108,44 +66,6 @@ function UserProducts() {
                             <p className="no-products-text">You have not added any products!</p>
                         </div>
                     )}
-                </div>
-                <div className="prod-by-type-div">
-                    {userProductsByType && Object.entries(userProductsByType).map(([productType, products]) => (
-                        <Collapsible 
-                            key={`${productType}-${products[0]}`} 
-                            label={productType} 
-                            className='prod-collapsible'
-                            isOpen={openCollapsible === productType}
-                            onToggle={() => handleCollapsibleClick(productType)}
-                            >
-                            <div className="prod-collapsible-content">
-                                {products.length > 0 ? (
-                                    products.map((product, index) => (
-                                        <div key={`${product}-${index}`}>
-                                            <OpenModalButton
-                                                className="prod-by-type-btn"
-                                                buttonText={
-                                                    <>
-                                                        <img 
-                                                            className="prod-by-type-img"
-                                                            src={product.preview_image} 
-                                                            alt={product.product_name}
-                                                            title={`${product.brand_name}-${product.product_name}`}
-                                                        />
-                                                    </>
-                                                }
-                                                modalComponent={<ProductInfoModal productId={product.id} />}
-                                            />
-                                        </div>
-                                    ))
-                                ) : (
-                                    <div>
-                                        <p>You have not added any {productType.toLowerCase()}!</p>
-                                    </div>
-                                )}
-                            </div>
-                        </Collapsible>
-                    ))}
                 </div>
             </div>        
         </div>
