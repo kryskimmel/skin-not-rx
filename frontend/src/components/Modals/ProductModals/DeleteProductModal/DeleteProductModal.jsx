8ab590570b8f1673775cb9209fc4f59d6de0db81
Krystal <kabkimmel@gmail.com>
2024-03-25 00:31:59 -0500

    changed style of span text

--- frontend/src/components/Modals/ProductModals/DeleteProductModal/DeleteProductModal.jsx
+++ frontend/src/components/Modals/ProductModals/DeleteProductModal/DeleteProductModal.jsx
@@ -32,7 +32,7 @@ function DeleteProductModal({brandName, productName, productId}) {
             <div className="delete-product-modal-div">
                 <Icon icon="ph:warning-duotone" color="red" width="50" height="50" />
                 <h1 className="delete-product-modal-heading">Confirm Delete</h1>
-                <h4 className="delete-product-modal-subheading">Are you sure you want to delete: <span style={{color:"#000435"}}>{brandName} {productName}</span>?</h4>
+                <h4 className="delete-product-modal-subheading">Are you sure you want to delete: <span style={{color:"#000435", fontWeight:"400"}}>{brandName} {productName}</span>?</h4>
                 <div className="delete-product-modal-buttons">
                     <button className="yes-button" onClick={handleYes}>Yes</button>
                     <button className="no-button" onClick={handleNo}>No</button>
