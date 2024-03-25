28e921dc82e02ad20dc659094a63c46d39fa0084
Krystal <kabkimmel@gmail.com>
2024-03-24 22:17:08 -0500

    modified view and add collection to favorites case in reducer

--- frontend/src/redux/favoriteCollection.js
+++ frontend/src/redux/favoriteCollection.js
@@ -54,28 +54,28 @@ const favoriteCollectionSlice = createSlice({
   extraReducers: (builder) => {
     builder
     .addCase(getFavoriteCollections.fulfilled, (state, action) => {
-        if (action.payload.FavoriteCollections) {
-          state.allFavoritedCollections = action.payload.FavoriteCollections || [];
-          state.byId = {};
-          action.payload.FavoriteCollections.forEach((faveCollection) => {
-            state.byId[faveCollection.id] = faveCollection;
-          })
-        }
-      })
-      .addCase(addCollectionToFavorites.fulfilled, (state, action) => {
-        const newFavoriteCollection = action.payload;
-        state.byId[newFavoriteCollection.id] = newFavoriteCollection;
-        if (!Array.isArray(state.allFavoritedCollections)) {
-          state.allFavoritedCollections = [];
-        }
-        state.allFavoritedCollections = [...state.allFavoritedCollections, newFavoriteCollection];
-      })
-      .addCase(removeCollectionFromFavorites.fulfilled, (state, action) => {
-        const favoriteCollectionId = action.payload;
-        delete state.byId[favoriteCollectionId];
-        state.allFavoritedCollections = state.allFavoritedCollections.filter(faveCollection => faveCollection.id !== favoriteCollectionId);
-      })
-    }
+      state.allFavoritedCollections = action.payload.FavoriteCollections || [];
+      state.byId = {};
+      if (Array.isArray(action.payload.FavoriteCollections)) {
+        action.payload.FavoriteCollections.forEach((faveColl) => {
+          state.byId[faveColl.id] = faveColl
+        })
+      }
+    })
+    .addCase(addCollectionToFavorites.fulfilled, (state, action) => {
+      const newFavoriteCollection = action.payload;
+      state.byId[newFavoriteCollection.id] = newFavoriteCollection;
+      if (!Array.isArray(state.allFavoritedCollections)) {
+        state.allFavoritedCollections = [];
+      }
+      state.allFavoritedCollections = [...state.allFavoritedCollections, newFavoriteCollection];
+    })
+    .addCase(removeCollectionFromFavorites.fulfilled, (state, action) => {
+      const favoriteCollectionId = action.payload;
+      delete state.byId[favoriteCollectionId];
+      state.allFavoritedCollections = state.allFavoritedCollections.filter(faveCollection => faveCollection.id !== favoriteCollectionId);
+    })
+  }
 });
 
 export default favoriteCollectionSlice;
