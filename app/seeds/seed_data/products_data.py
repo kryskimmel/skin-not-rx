5a1f43f820551555d1a111b37cfb3ac2536e930f
Krystal <kabkimmel@gmail.com>
2024-03-26 15:42:16 -0500

    updated seed data preview image links

--- app/seeds/seed_data/products_data.py
+++ app/seeds/seed_data/products_data.py
@@ -8,7 +8,7 @@ products_data = [
        "description": "A lightweight water-based moisturizer that provides intense hydration.",
        "key_ingredients": "Hyaluronic Acid, Glycerin, Olive Extract",
        "product_link": "https://www.neutrogena.com/products/skincare/neutrogena-hydro-boost-water-gel-with-hyaluronic-acid/6811047.html",
-       "preview_image": "https://images.heb.com/is/image/HEBGrocery/001828183-4?jpeg",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/neutrogena_hyrdroboost_watergel.jpeg",
        "user_id": 1
     },
    #  #2
@@ -19,7 +19,7 @@ products_data = [
        "description": "Broad-spectrum SPF 60 sunscreen with a velvety texture for face and body.",
        "key_ingredients": "La Roche-Posay Thermal Spring Water, Vitamin E, Cell-Ox Shield® Technology",
        "product_link": "https://www.laroche-posay.us/our-products/sun/body-sunscreen/anthelios-melt-in-milk-sunscreen-spf-60-antheliosmeltinmilk.html",
-       "preview_image": "https://crdms.images.consumerreports.org/prod/products/cr/models/403107-lotion-la-roche-posay-anthelios-melt-in-milk-sunscreen-spf-60-10025494.png",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/la-roche-posay_anthelios-melt-in-milk-sunscreen-spf.png",
        "user_id": 1
     },
    #  #3
@@ -30,7 +30,7 @@ products_data = [
        "description": "A gel moisturizer packed with glow-boosting Cherry Blossom Extracts, visibly brightening niacinamide, and hydrating betaine from sugar beets.",
        "key_ingredients": "Cherry Blossom Flavonoids, Niacinamide, Sugar Beet Betaine",
        "product_link": "https://us.innisfree.com/products/dewy-glow-jelly-cream-with-jeju-cherry-blossom-2",
-       "preview_image": "https://us.innisfree.com/cdn/shop/products/DewyGlowJellyCream-1080x1080.jpg",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/innisfree_dewy-moisturizer.jpeg",
        "user_id": 1
     },
    #  #4
@@ -41,7 +41,7 @@ products_data = [
        "description": "A gentle, nourishing cleanser that restores hydration and antioxidants to the skin.",
        "key_ingredients": "Matcha, Hemp Seed Oil, Sodium PCA",
        "product_link": "https://kravebeauty.com/products/matcha-hemp-hydrating-cleanser",
-       "preview_image": "https://i.ebayimg.com/images/g/QG4AAOSwolNkHSd6/s-l960.webp",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/krave_beautry_matcha-hemp-cleanser.webp",
        "user_id": 1
     },
    #  #5
@@ -52,7 +52,7 @@ products_data = [
        "description": "A peptide-rich, multifunctional serum helps reduce the visible signs of aging and keeps skin looking healthy.",
        "key_ingredients": "Matrixyl®3000, SYN®-AKE, Organic Green Tea",
        "product_link": "https://www.getjackblack.com/protein-booster-skin-serum.aspx#",
-       "preview_image": "https://www.getjackblack.com/Images/2012_ProteinBoosterSerum_2oz_WEB.png",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/jack_black_skin-serum.png",
        "user_id": 1
     },
    #  #6
@@ -63,7 +63,7 @@ products_data = [
        "description": "This clinically effective formula is inspired from our original cult-fave Watermelon Glow Sleeping Mask.",
        "key_ingredients": "2.5% pH-Balanced AHA Complex, Hyaluronic Acid, Niacinamide + Quinoa Peptides",
        "product_link": "https://www.glowrecipe.com/products/watermelon-glow-aha-night-treatment",
-       "preview_image": "https://i1.wp.com/blog.glowrecipe.com/wp-content/uploads/2022/08/glow_watermelonglow_0272.jpg",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/glow_recipe_aha-glow-treatment.jpeg",
        "user_id": 1
     },
    #  #7
@@ -74,7 +74,7 @@ products_data = [
        "description": "This intense hydration mask is a cooling bio-cellulose mask is saturated in saccharomyces. ",
        "key_ingredients": "Hyaluronic Acid, Watermelon Extract, Aloe Leaf Extract",
        "product_link": "https://saturdayskin.com/products/hydration-mask?variant=39796122255418",
-       "preview_image": "https://media1.popsugar-assets.com/files/thumbor/31yDWZmCIjQ4ZR1ibEKUt2DRZIE=/fit-in/2000x2000/filters:format_auto():extract_cover():upscale()/2018/11/20/806/n/1922153/fe99c7d3643b6bc6_netimgGLj2vx.jpg",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/saturday_skin_intense-hydration-mask.jpeg",
        "user_id": 1
     },
    # #  #8
@@ -85,7 +85,7 @@ products_data = [
        "description": "This wash-off mask starts as a smooth gel and tightens as it dries, leaving your skin visibly lifted and firmer. ",
        "key_ingredients": "Red Wine Extract, Centella Asiatica Extract",
        "product_link": "https://tonymoly.us/collections/skincare-masks/products/im-red-wine-pore-tightening-mask",
-       "preview_image": "https://tonymoly.us/cdn/shop/products/I_mRedWinePoreTighteningMask_1200x.png",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/tony_moly_im-red-wine-tightening-mask.png",
        "user_id": 1
     },
    #  #9
@@ -96,7 +96,7 @@ products_data = [
        "description": "Depuff, brighten, hydrate, and reduce the appearance of fine lines and wrinkles with this powerful yet gentle eye serum! ",
        "key_ingredients": "Snail Secretion Filtrate, Niacinamide, Centella Asiatica (Cica) Extract",
        "product_link": "https://tonymoly.us/collections/eye-care-eye-cream/products/intense-care-gold-24k-snail-eye-serum-ball",
-       "preview_image": "https://tonymoly.us/cdn/shop/files/IntenseCareGold24KSnailEyeSerumBall01_2048x.png",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/tony_moly_intense-care-eye-serum.png",
        "user_id": 1
     },
    # #  #10
@@ -107,7 +107,7 @@ products_data = [
        "description": "Our best-selling anti-aging eye cream now visibly reduces wrinkles and diminishes the appearance of dark circles.",
        "key_ingredients": "Peptides, Squalane, Ginseng Extract",
        "product_link": "https://www.shiseido.com/us/en/benefiance-wrinkle-smoothing-eye-cream-0768614208570.html?cgid=skincare",
-       "preview_image": "https://shiseido.ipscdn.net/sa2/dw/image/v2/BBSK_PRD/on/demandware.static/-/Sites-itemmaster_shiseido/default/dw495b4d42/images/2022/July/Top25/0730852155794_1.jpg",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/shiseido_wrinkle-smoothing-eye-cream.jpeg",
        "user_id": 1
     },
    # #  #11
@@ -118,7 +118,7 @@ products_data = [
        "description": "An oil-serum hybrid that's a one-step shortcut to the way your skin looks after a full skincare routine.",
        "key_ingredients": "Jojoba Oil, Grape Seed Oil, Sugarcane Squalene",
        "product_link": "https://www.glossier.com/products/futuredew",
-       "preview_image": "https://glossier-prod.imgix.net/products/glossier-futuredew-carousel-01.png",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/glossier_futuredew.png",
        "user_id": 1
     },
    # #  #12
@@ -129,7 +129,7 @@ products_data = [
        "description": "THAYERS Rose Petal Facial Toner visibly improves the quality of skin, revealing glowy, moisturized skin that looks and feels healthier.",
        "key_ingredients": "Rose Water, Witch Hazel, Aloe Vera",
        "product_link": "https://www.thayers.com/ROSE-PETAL-FACIAL-TONER/THY0003.html",
-       "preview_image": "https://i5.walmartimages.com/seo/Thayers-Witch-Hazel-Rose-Petal-Facial-Toner-8-5-fl-oz_acd37c3e-a0bf-42e6-8728-36b770844c62.73ee664cd16feacbf52f076e68b21b6d.jpeg",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/thayers_rose-petal-facial-toner.jpeg",
        "user_id": 1
     },
    # #  #13
@@ -140,7 +140,7 @@ products_data = [
        "description": "This serum is made for those struggling with enlarged pores and skin inflammation. ",
        "key_ingredients": "Propolis Extract, Niacinamide",
        "product_link": "https://beautyofjoseon.com/products/glow-serum-propolis-niacinamide?variant=40070016237733",
-       "preview_image": "https://www.shopbeautydepot.com/cdn/shop/products/beauty-of-joseon-face-care-beauty-of-joseon-glow-serum-30ml-30641082761302_1024x1024.jpg",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/beauty-of-joseon_glow-serum.jpeg",
        "user_id": 1
     },
    # #  #14
@@ -151,7 +151,7 @@ products_data = [
        "description": "This intensive balm gives your lips a serious, long-lasting moisture that immediately soothes and helps strengthen your lips' delicate moisture barrier.",
        "key_ingredients": "Shea Butter",
        "product_link": "https://www.elizabetharden.com/products/eight-hour-cream-intensive-lip-repair-balm",
-       "preview_image": "https://images-eu.ssl-images-amazon.com/images/I/71YDYKb9rhL._AC_UL600_SR600,600_.jpg",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/elizabeth-arden_lip-repair-balm.jpeg",
        "user_id": 1
     },
    # #  #15
@@ -162,7 +162,7 @@ products_data = [
        "description": "Light-weight essence which absorbs into the skin fast to give skin a natural glow from the inside.",
        "key_ingredients": "Snail Secretion Filtrate, Sodium Hyaluronate",
        "product_link": "https://www.cosrx.com/products/advanced-snail-96-mucin-power-essence",
-       "preview_image": "https://cdn.shopify.com/s/files/1/0403/8200/5401/files/Advanced_Snail_96_Mucin_Power_Essence_2.jpg",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/cosrx_snail-mucin-essence.jpeg",
        "user_id": 1
     },
    # #  #16
@@ -173,7 +173,7 @@ products_data = [
        "description": "A gently effective, cream cleanser that washes away daily buildup without stripping skin for a soft, luminous complexion",
        "key_ingredients": "PH-Neutral Amino Acid Base, Japanese Rice Powder, Okinawa Algae Blend + Hyaluronic Acid",
        "product_link": "https://www.tatcha.com/product/rice-wash-creamy-rice-powder-cleanser/CC03011T.html",
-       "preview_image": "https://i.pinimg.com/736x/f8/4e/b2/f84eb2dc5f3955132ac7b4791d2fa4ed.jpg",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/tatcha_the-rice-wash.jpeg",
        "user_id": 1
     },
    # #  #17
@@ -184,7 +184,7 @@ products_data = [
        "description": "A leave-on lip sleeping mask that delivers ultra-hydrating and nourishing benefits, kissing flakey lips goodbye!",
        "key_ingredients": "Shea Butter, Murumuru Butter, Vitamin C, Coconut Oil",
        "product_link": "https://us.laneige.com/products/lip-sleeping-mask",
-       "preview_image": "https://viktoriyabeauty.com/cdn/shop/products/Instagram_3_900x900.png",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/laneige_lip-mask.png",
        "user_id": 1
     },
    # #  #18
@@ -195,7 +195,7 @@ products_data = [
        "description": "A deep-cleansing, non-drying creamy clay mask—formulated with absorbent Jeju Super Volcanic Clusters™ and AHA—that helps clear and visibly minimize the look of pores while it exfoliates.",
        "key_ingredients": "Jeju Super Volcanic Clusters™",
        "product_link": "https://us.innisfree.com/products/pore-clearing-clay-mask-2x-with-super-volcanic-clusters",
-       "preview_image": "https://us.innisfree.com/cdn/shop/products/VCMaks_20ml.jpg",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/innisfree_pore-clearing-mask.jpeg",
        "user_id": 1
     },
    # #  #19
@@ -206,7 +206,7 @@ products_data = [
        "description": "An everyday cream moisturizer that uses natural ingredients to deeply hydrate and restore essential nutrients for dry to normal skin.",
        "key_ingredients": "Aloe Leaf Juice, Vitamin E, Avocado Oil",
        "product_link": "https://hellobubble.com/products/slam-dunk-hydrating-moisturizer",
-       "preview_image": "https://hellobubble.com/cdn/shop/products/slamdunk_01_38cf4528-da30-43a5-959c-2d63413febdf.jpg",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/bubble_slam-dunk-hydrating-moisturizer.jpeg",
        "user_id": 1
     },
    # #  #20
@@ -217,7 +217,7 @@ products_data = [
        "description": "An energizing, non-greasy men's face moisturizer. Paraben-free & sulfate-free.",
        "key_ingredients": "Chestnut Extract, Vitamin C, Caffeine",
        "product_link": "https://www.kiehls.com/mens/mens-moisturizers/facial-fuel-daily-energizing-moisture-treatment-for-men/555.html",
-       "preview_image": "https://bluemercury.com/cdn/shop/products/global_images-3605970407964-1_1100x.jpg",
+       "preview_image": "https://skin-not-rx-bucket.s3.us-east-2.amazonaws.com/product-seed-images/kiehls_facial-fuel-daily-energizing-moisture-treatment-for+-men.jpeg",
        "user_id": 1
     },
 ]
